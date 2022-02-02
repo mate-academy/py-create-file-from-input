da59d79e80595b2df3d67345ddd079a181f94822
roland813 <olegkorostensky@gmail.com>
2022-02-02 19:15:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def file_from_input():
+    name = input("Enter name of the file: ")
+    is_runing = True
+    while is_runing:
+        content = input("Enter new line of content: ")
+        if content == "stop":
+            is_runing = False
+        else:
+            with open(f"{name}.txt", "a") as f:
+                f.write(f"{content}\n")
+
+
+if __name__ == '__main__':
+    file_from_input()

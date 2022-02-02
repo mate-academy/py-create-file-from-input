5e39db2fc0269fa6bf6155b542cbe1d3e512a962
Artur Shavula <arthuromgnh@gmail.com>
2022-02-02 13:56:16 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def get_file_content():
+    message = 'Enter new line of content: '
+    content = []
+    while (line := input(message)) != 'stop':
+        content.append(line + '\n')
+    return content
+
+
+def create_file():
+    file_name = input('Enter name of the file:')
+    lines = get_file_content()
+    with open(file=file_name + '.txt', mode='w') as fout:
+        fout.writelines(lines)
+
+
+if __name__ == '__main__':
+    create_file()

4217c699d59f534022a2cf7cfaa21f3b95b89052
Alex Gergel <alexgergel@gmail.com>
2022-02-02 16:29:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+file_name = input("Please enter the name of the file: ")
+file_content = ""
+
+while True:
+    new_line = input("Waiting for input: ")
+    if new_line == "stop":
+        break
+    else:
+        file_content += new_line + "\n"
+
+with open(file_name, "w") as f:
+    f.writelines(file_content)

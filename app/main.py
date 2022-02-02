21b5bed64867bdbcb173f898881c6bd1d4034b0d
Nazar-kob <kobrinnazar05@mail.com>
2022-02-02 14:29:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def create_a_file():
+    name = input("Enter name of the file: ")
+    with open(name + '.txt', 'w') as file:
+        while True:
+            content = input("Enter new line of content: ")
+            if content == "stop":
+                break
+            file.writelines(content + '\n')
+
+
+create_a_file()

8ab57a690745a9ccbac373cd5a5fd931f7f61f47
Rybak Dmytro <codingclaud@gmail.com>
2022-02-02 14:13:21 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def user_management_cli():
+    file_name = input("Input the name of file: ")
+    print("File content: ")
+
+    while True:
+        content = input()
+
+        if content.lower() == "stop":
+            break
+        else:
+            with open(file_name, "a") as f:
+                f.write(content + "\n")
+
+
+if __name__ == "__main__":
+    user_management_cli()

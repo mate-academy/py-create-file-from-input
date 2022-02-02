59edc04fa354a82c609faf675613a29ac0de9039
TJgkOsVP <sasha06052001@gmail.com>
2022-02-02 15:50:29 +0200

    'solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_files():
+    while True:
+        file_name = input("Enter name of the file: ")
+        if file_name:
+            with open(file_name + ".txt", "a") as f:
+                content = input("Enter new line of content: ")
+                while content != "stop":
+                    content = input("Enter new line of content: ")
+                    f.write(content + "\n")
+                print("Have a nice day!")
+                break
+
+
+if __name__ == '__main__':
+    create_files()

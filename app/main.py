55c6f7c1818e1e98779699ecf5fe600c9f571557
alix makarova <alix.makarova@gmail.com>
2022-02-02 14:06:59 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def create_a_file():
+    name_of_file = input("Please, enter the file name: ")
+    name_of_file = name_of_file + ".txt"
+    with open(name_of_file, "w") as file:
+        while True:
+            line = input('Please, enter the line of the content '
+                         'or "stop" for stop the program: ')
+            if line == "stop":
+                print("Stopped")
+                break
+            file.write(line + "\n")

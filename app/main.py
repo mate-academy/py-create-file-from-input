cbcddd212454dc560eeea8f447d98f89831ec082
Alex Zhidkov <sashazudkov.@gmail.com>
2022-02-02 14:09:36 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,26 +1,19 @@
-class FileTxtError(Exception):
-    pass
+def create_name_file(name_file, input_line):
+    with open(name_file + '.txt', 'w') as file:
+        file.writelines(input_line)
 
 
 def app():
-    name_file = ''
-    while True:
-        try:
-            name_file = input("Enter name of the file: ")
-            if not name_file[len(name_file) - 4:] == ".txt":
-                raise FileTxtError
-        except FileTxtError:
-            print("File only with this extension can be created: .txt")
-            continue
-        else:
-            break
+    content = []
+    name_file = input("Enter name of the file: ")
     while True:
         input_line = input("Enter new line of content: ")
         if input_line == 'stop':
             break
         else:
-            with open(name_file, 'a') as file:
-                file.write(f'{input_line}\n')
+            content.append(input_line + '\n')
+
+    create_name_file(name_file, content)
 
 
 if __name__ == "__main__":

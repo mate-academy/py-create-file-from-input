522cb4c654ff12677096c6f8c680b65c6c2bf4f2
Zikkolion <nmadaraosnova835@gmail.com>
2022-02-03 09:53:45 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def create_file_line():
+    file_name = input("Please, enter the file name: ")
+
+    while True:
+        line = input("Please, enter new line of content: ")
+        if line == "stop":
+            break
+        else:
+            with open(file_name + ".txt", "a") as f:
+                f.write(f"{line}\n")
+
+
+if __name__ == "__main__":
+    create_file_line()

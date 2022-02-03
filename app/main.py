3e18032e3f82866c4d6c683bb7e2e8b88a42786a
Arthur Shavula <52936635+Krimat@users.noreply.github.com>
2022-02-03 11:43:20 +0200

    Update app/main.py
    Co-authored-by: Ivan Ramyk <ivan.ramyk.official@gmail.com>

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def get_file_content():
 
 
 def create_file():
-    file_name = input('Enter name of the file:')
+    file_name = input('Enter name of the file: ')
     lines = get_file_content()
     with open(file=file_name + '.txt', mode='w') as fout:
         fout.writelines(lines)

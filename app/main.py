16454d2155f1a47f79fd3c192a1f21807121b9f7
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-02-03 06:32:36 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,16 +1,11 @@
 def user_file():
-    usercontent = []
-    filename = ""
-    while len(filename) == 0 :
-        filename = input("Enter name of the file: ")
+    filename = input("Enter name of the file: ")
     while True:
         content = input("Enter new line of content: ")
         if content == "stop":
             break
-        usercontent.append(content)
-    with open(f"{filename}.txt", "a") as f:
-        for cont in usercontent:
-            f.write(f"{cont}\n")
+        with open(f"{filename}.txt", "a") as f:
+            f.write(f"{content}\n")
 
 
 if __name__ == '__main__':

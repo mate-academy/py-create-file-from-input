c612ffa6cf9986888aa9d8c0aa7fc73aaa991a06
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-05-12 17:04:42 +0300

    Refactor code

--- app/main.py
+++ app/main.py
@@ -1,12 +1,9 @@
 file_name = input("Please, enter File name : ")
 
-lines = []
-line = ""
-while line != "stop":
-    line = input("Enter new line of content: ")
-    lines.append(line)
-
 with open(f"{file_name}.txt", "w", encoding="utf8") as file:
-    for line in lines:
+    line = ""
+    while line != "stop":
+        line = input('Enter new line of content: ')
         file.write(f"{line}\n")
-    print(f"Recording file - {file_name}.txt - is success")
+
+print(f"Recording file - {file_name}.txt - is success")

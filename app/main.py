9682b51acf8be6f6da08f3a1b5c95e02b986293c
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-05-12 16:55:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+file_name = input("Please, enter File name : ")
+
+lines = []
+line = ""
+while line != "stop":
+    line = input("Enter new line of content: ")
+    lines.append(line)
+
+with open(f"{file_name}.txt", "w", encoding="utf8") as file:
+    for line in lines:
+        file.write(f"{line}\n")
+    print(f"Recording file - {file_name}.txt - is success")

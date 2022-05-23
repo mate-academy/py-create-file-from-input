c4700768d5b85830a86605249b074a4fd3f82904
Marko Khodan <khodanmarko@gmail.com>
2022-05-23 23:43:24 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+name = input("Enter file name: ")
+with open(f"{name}.txt", "a") as f:
+    user_string = 1
+    while user_string != "stop":
+        user_string = input("Enter string to add: ")
+        if user_string != "stop":
+            f.write(f"{user_string}\n")

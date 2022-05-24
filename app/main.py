078dbfe449646d71d33c93027cb8395f43af72e0
Marko Khodan <khodanmarko@gmail.com>
2022-05-24 15:12:41 +0300

    variable name fixed

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 file_name = input("Enter file name: ")
-with open(f"{file_name}.txt", "a") as f:
+with open(f"{file_name}.txt", "a") as file:
     user_string = ""
     while user_string != "stop":
         user_string = input("Enter string to add: ")
         if user_string != "stop":
-            f.write(f"{user_string}\n")
+            file.write(f"{user_string}\n")

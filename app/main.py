05f26c0ad4bdf4e96e4733051e90a90ed6b631f4
Flashmobber <ufinaid@gmail.com>
2022-05-27 22:46:05 +0300

    All done.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+file = ""
+
+while len(file) == 0:
+    file = input("Enter name of the file: ")
+    file = file.strip()
+
+if ".txt" not in file:
+    file += ".txt"
+
+with open(file, "w") as f:
+    while True:
+        content = input("Enter new line of content: ")
+        if content == "stop":
+            break
+        f.write(content + "\n")
+    f.close()

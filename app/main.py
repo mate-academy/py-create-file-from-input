1a1e8eed7d619588c1cf1d2c30cee18067b547ff
Serhii Leonenko <leonenko3149@gmail.com>
2022-05-27 20:29:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+file_name = input("Enter name of the file: ")
+
+with open(file_name, "w") as f:
+    while True:
+        content = input("Enter new line of content: ")
+        if content == "stop":
+            break
+        f.write(content + "\n")

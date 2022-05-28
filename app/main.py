65db842eb17536a988c95dae8752dc1b450a1ac1
Taras Nester <tarasnester414@gmail.com>
2022-05-28 15:14:00 +0300

    base version

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+file_name = input("Enter file name:") + ".txt"
+
+with open(file_name, "w") as file:
+    while True:
+        line = input("Write new line of content:")
+        if line.lower() == "stop":
+            break
+        file.write(line + "\n")

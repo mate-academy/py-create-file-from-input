30e97bc9a151fff2d03d26802d1c2572ce573060
Yurii Kindrat <yurkinkek@gmail.com>
2022-05-28 14:12:32 +0300

    Solutin

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def file_creator():
+    print("Enter name of file:")
+    name_of_file = input()
+    with open(f'{name_of_file}.txt', 'a') as f:
+        while True:
+            print("Enter new line of content:")
+            content = input()
+            if content == 'stop':
+                break
+            f.write(f"{content}\n")
+
+
+file_creator()

5d8a98abfa48878779a52014f981fe92b2137509
Dima Svirsa <august4smile@gmail.com>
2022-05-29 11:24:58 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+file_name = input("Enter file name you want to create:") + ".txt"
+with open(file_name, "a") as file_add_info:
+    while True:
+        line = input("Enter file content or 'stop' to exit input:")
+        if line == "stop":
+            break
+        file_add_info.write(f"{line}\n")

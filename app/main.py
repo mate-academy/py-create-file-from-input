992177fb8a7a6f8fcd0a1c8bffa4018217dbe5db
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-05-30 14:59:22 +0300

    'MinorFix'

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ changed_name = client_input + '.txt'
 f = open(changed_name, 'w')
 while True:
     client_file_input = input('Please add some context')
-    f.write(client_file_input + '\n')
     if client_file_input == 'stop':
         f.close()
         break
+    f.write(client_file_input + '\n')
+

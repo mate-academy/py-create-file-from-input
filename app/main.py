b1bc42032f34fb70c95e6dd87bfd2d37c45006f7
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-05-30 14:31:45 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+a = input('Please enter the name of file')
+c = a + '.txt'
+f = open(c, 'w')
+while True:
+    b = input('Please add some context')
+    if b == 'stop':
+        break
+    f.write(b)
+    f.write(' ')

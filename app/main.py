691a9bd5233a0d6695dd96454c10f9cb037a12af
Oleksii Isaiev <isaev.ieskela@gmail.com>
2022-05-30 17:47:49 +0300

    'Refactor'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-file_name = input("Enter name of the file: ") + ".txt"
-with open(file_name, "a") as file:
-    while True:
-        line_of_content = input("Enter new line of content: ")
-        if line_of_content == "stop":
-            break
-        file.write(f"{line_of_content}\n")
+def main():
+    file_name = input("Enter name of the file: ") + ".txt"
+    with open(file_name, "a") as file:
+        while True:
+            line_of_content = input("Enter new line of content: ")
+            if line_of_content == "stop":
+                break
+            file.write(f"{line_of_content}\n")

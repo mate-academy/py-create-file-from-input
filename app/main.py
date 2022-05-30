946f4050882dcf71562100ea2ea8c441df70aa15
ILIAS <nossudak@gmail.com>
2022-05-30 09:52:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+file_name = input("Enter name of the file: ") + ".txt"
+with open(file_name, 'a') as file:
+    while True:    # will stop when user enters "stop"
+        line = input("Enter new line of content: ")
+        if line == "stop":
+            break
+        file.write(line + "\n")

7ee3d8629c9cf2862d4e37060f45d01c6da4407d
Roma Tsybulia <Romalonstejn@gmail.com>
2022-05-30 15:40:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+file_name = input("Enter name of the file: ") + ".txt"
+lines_list = []
+
+while True:
+    content = input("Enter new line of content:")
+    if content == "stop":
+        break
+    lines_list.append(content)
+
+with open(file_name, "x") as file:
+    for line in lines_list:
+        file.write(line + "\n")

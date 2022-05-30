aa4a2d0bba5f60b9a408c8e86d281fa230acb875
Yurii Kindrat <103201010+YuriiKindrat@users.noreply.github.com>
2022-05-30 15:53:29 +0300

    changed the mode in open function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def file_creator():
     name_of_file = input("Enter name of the file: ")
-    with open(f'{name_of_file}.txt', 'a') as f:
+    with open(f'{name_of_file}.txt', 'w') as f:
         while True:
             content = input("Enter new line of content: ")
             if content == 'stop':

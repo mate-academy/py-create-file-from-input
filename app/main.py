421a8795f44ad575f2649c1ef3514ef2b75b797c
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-05-30 17:52:44 +0300

    'The_test_was_added'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-client_input = input('Please enter the name of file')
-changed_name = client_input + '.txt'
-f = open(changed_name, 'w')
-while True:
-    client_file_input = input('Please add some context')
-    if client_file_input == 'stop':
-        f.close()
-        break
-    f.write(client_file_input + '\n')
 
+def main():
+    client_input = input('Enter name of the file: ')
+    changed_name = client_input + '.txt'
+    f = open(changed_name, 'w')
+    while True:
+        client_file_input = input('Enter new line of content: ')
+        if client_file_input == 'stop':
+            f.close()
+            break
+        f.write(client_file_input + '\n')

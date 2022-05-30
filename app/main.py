d018585082a28f1154115cffd71cdd0595bee4e0
Taras Nester <tarasnester414@gmail.com>
2022-05-30 17:20:05 +0300

    fixed func, and messages

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def main():
-    file_name = input("Enter file name:") + ".txt"
+    file_name = input("Enter name of the file: ") + ".txt"
 
     with open(file_name, "w") as file:
         while True:
-            line = input("Write new line of content:")
+            line = input("Enter new line of content: ")
             if line.lower() == "stop":
                 break
             file.write(line + "\n")

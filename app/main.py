05f630bb3bf31dd00a99d74f8bf895184c5d81a9
Alexander <alexzubkoff123@gmail.com>
2022-10-01 21:11:34 +0300

    App for creating file with content from user input

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 def main():
-    # write your code here
-    pass
+    input_str = str(input("Enter name of the file: "))
+    if input_str:
+        file_name = input_str
+        f = open(f"{file_name}.txt", "w")
+        while True:
+            input_str = str(input("Enter new line of content: "))
+            if input_str != "stop":
+                f.write(input_str + "\n")
+            else:
+                break
+        f.close()
 
 
 if __name__ == "__main__":

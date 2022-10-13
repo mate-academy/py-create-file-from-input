d092ed76e140140755e9b0848ef977cc7cb637dd
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-10-13 12:31:59 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
-def main():
-    # write your code here
-    pass
+def main() -> str:
+    file_name = input("Enter name of the file: ")
+    new_file_line = ""
+    with open(f"{file_name}.txt", "a") as f:
+        while new_file_line != "stop":
+            new_file_line = input("Enter new line of content: ")
+            if new_file_line == "stop":
+                break
+            f.write(f"{new_file_line}\n")
 
 
 if __name__ == "__main__":

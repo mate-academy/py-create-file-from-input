19bf57bc0d325f84a41fd133475bae0cdbf3277c
Nataliia <amirammina@gmail.com>
2022-10-16 14:41:25 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
-def main():
-    # write your code here
-    pass
+class FileManager:
+    def __init__(self) -> None:
+        self.name = input("Enter name of the file: ")
+        self.mode = "a"
+        self.file = None
+
+    def __enter__(self) -> object:
+        self.file = open(f"{self.name}.txt", self.mode)
+        return self.file
+
+    def __exit__(self, exc_type: str, exc_val: int, exc_tb: str) -> None:
+        self.file.close()
+
+
+def main() -> None:
+    with FileManager() as curr_file:
+        while True:
+            new_line = input("Enter new line of content: ")
+            if new_line == "stop":
+                break
+            curr_file.write(f"{new_line}\n")
 
 
 if __name__ == "__main__":

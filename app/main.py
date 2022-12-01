0710fea073034ae5f846a7e2143cc071287b1251
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-01 15:02:53 +0200

    change main

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 def main() -> None:
     file_name = input("Enter name of the file: ")
-    with open(file_name, "a") as file:
+    text = input("Enter new line of content: ")
+    with open(file_name, "w") as file:
         while True:
             text = input("Enter new line of content: ")
             file.write(text + "\n")
             if text == "stop":
                 break
+    with open(f"{file_name}.txt", "r") as f:
+        print(f"File name: {file_name}.txt\nFile content:")
+        print(f.read())
 
 
 if __name__ == "__main__":

a29649dd07bf29a41df43418804b91b8c99f45df
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-01 14:48:09 +0200

    created def main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 def main():
-    # write your code here
-    pass
-
+    file_name = input("Enter name of the file: ")
+    with open(file_name, "a") as file:
+        while True:
+            text = input("Enter new line of content:")
+            file.write(text + "\n")
+            if text == "stop":
+                break
 
 if __name__ == "__main__":
     main()

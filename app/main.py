3f7f0b347e244216439a29f84c0f1736d0431632
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-01 14:50:08 +0200

    flake8 1

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def main():
     file_name = input("Enter name of the file: ")
     with open(file_name, "a") as file:
         while True:
-            text = input("Enter new line of content:")
+            text = input("Enter new line of content: ")
             file.write(text + "\n")
             if text == "stop":
                 break

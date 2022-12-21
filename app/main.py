8ec3d99a7ed8691ecb3117cbcbdfd690e04aa26b
Yevhen Martyniuk <evgenijmartynuk07@gmail.com>
2022-12-21 20:16:09 +0300

    'soll'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 def main():
-    # write your code here
-    pass
+    name_file = input("Enter name of the file: ") + ".txt"
+    with open(name_file, "a") as file:
+        while True:
+            write = input("Enter new line of content: ")
+            if write == "stop":
+                break
+            file.write(write + '\n')
 
 
 if __name__ == "__main__":

18b92e8b59fe874e4f167d1ae440f032ab7e886c
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-12 12:23:33 +0200

    2 commit

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main():
         content = input("Enter new line of content: ")
         if content != "stop":
             result.append(content + "\n")
-    with open(file_name, "a") as file:
+    with open(file_name + ".txt", "w") as file:
         file.writelines(result)
 
 

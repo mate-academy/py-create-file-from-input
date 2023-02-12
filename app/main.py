48e7dd686c9791f021100590625139b2090dfdc6
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-12 12:31:57 +0200

    use writelines

--- app/main.py
+++ app/main.py
@@ -2,15 +2,13 @@ def main() -> None:
     content = []
     file_name = input("Enter name of the file: ")
     full_name = file_name + ".txt"
-    stop = "stop"
     while True:
         new_line = input("Enter new line of content: ")
-        if new_line == stop:
+        if new_line == "stop":
             break
         content.append(new_line + "\n")
     with open(full_name, "a") as f:
-        for line in content:
-            f.writelines(line)
+        f.writelines(content)
 
 
 if __name__ == "__main__":

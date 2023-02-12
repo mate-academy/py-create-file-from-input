7dfb760e2bc1cab5c0771ccfe4545fa177ad4f4e
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-12 12:42:58 +0200

    contex == type(str)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 def main() -> None:
-    content = []
+    content = ""
     file_name = input("Enter name of the file: ")
     full_name = file_name + ".txt"
     while True:
         new_line = input("Enter new line of content: ")
         if new_line == "stop":
             break
-        content.append(new_line + "\n")
+        content += new_line + "\n"
     with open(full_name, "a") as f:
         f.writelines(content)
 

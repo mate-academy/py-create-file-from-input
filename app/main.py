504b2d4cdfaa2bdc6e3729d62f4ea988e3e6cae6
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-02-19 13:34:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def main() -> None:
     file_name = input("Enter name of the file: ")
     with open(f"{file_name}.txt", "w") as f:
-
         is_running = True
         while is_running:
             new_line = input("Enter new line of content: ")

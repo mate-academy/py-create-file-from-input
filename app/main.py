859e153f1752e1bca1579068b3551ce5b83404e5
Misha Vovchok <mivvaw@gmail.com>
2023-02-20 17:15:31 +0200

    sol

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 def main():
-    # write your code here
-    pass
+    file_name = input("Enter name of the file: ")
+
+    content = []
+    txt = ""
+
+    while txt != "stop":
+        txt = input("Enter new line of content: ")
+        if txt != "stop":
+            content.append(txt)
+
+    with open(f"{file_name}.txt", "a") as file:
+        for line in content:
+            file.write(f"{line}\n")
 
 
 if __name__ == "__main__":

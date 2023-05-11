5aca6b54d40cb0db4746202f767ad8187a2c3ffc
admin <admin@Yaroslavs-MacBook-Pro.local>
2023-05-11 10:56:58 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import os
+
+
 def main() -> None:
     file_name = input("Enter name of the file: ")
     file_name += ".txt"
@@ -7,6 +10,12 @@ def main() -> None:
         while new_line != "stop":
             f.write("\n" + new_line)
             new_line = input("Enter new line of content: ")
+
+    if os.path.isfile(file_name) and os.stat(file_name).st_size > 0:
+        print(f"File '{file_name}' has been created.")
+
+    else:
+        print(f"File '{file_name}' is empty or does not exist.")
 
 
 if __name__ == "__main__":

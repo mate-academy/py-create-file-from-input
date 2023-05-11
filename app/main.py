fda6258086bbd96686911a0c19972f56be4bb749
admin <admin@Yaroslavs-MacBook-Pro.local>
2023-05-11 11:08:29 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -6,10 +6,14 @@ def main() -> None:
     file_name += ".txt"
     with open(file_name, "a") as f:
         new_line = input("Enter new line of content: ")
+        if new_line == "stop":
+            return
         f.write(new_line)
-        while new_line != "stop":
-            f.write("\n" + new_line)
+        while True:
             new_line = input("Enter new line of content: ")
+            if new_line == "stop":
+                break
+            f.write("\n" + new_line)
 
     if os.path.isfile(file_name) and os.stat(file_name).st_size > 0:
         print(f"File '{file_name}' has been created.")

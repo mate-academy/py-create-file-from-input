f9341b2a6f5deaa9f76929134acff0b8714c0898
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-05-19 00:13:32 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 def main() -> None:
     file_name = input("Enter file name: ")
     file_name += ".txt"
-    file = open(file_name, "w")
-    with open(file_name, "a") as file:
+    file_text = open(file_name, "w")
+    with open(file_name, "a") as file_text:
         while True:
             content = input("Enter new line of content: ")
             if content == "stop":
                 break
             else:
-                file.write(content)
-                file.write("\n")
+                file_text.write(content)
+                file_text.write("\n")
 
 
 if __name__ == "__main__":

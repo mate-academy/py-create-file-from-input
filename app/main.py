d90077e778ed4c8f01f5318f121f89d57d984ef1
Glieb <gleblitvinenko3007@gmail.com>
2023-05-31 08:51:56 +0300

    Soution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+    content = []
+
+    while True:
+        line = input("Enter new line of content: ")
+        if line.lower() == "stop":
+            break
+        content.append(line)
+
+    file_path = file_name + ".txt"
+    with open(file_path, "w") as file:
+        for line in content:
+            file.write(line + "\n")
+
+    print(f'File name: \"{file_path}\"')
+    print("File content:")
+    for line in content:
+        print("#", line)
 
 
 if __name__ == "__main__":

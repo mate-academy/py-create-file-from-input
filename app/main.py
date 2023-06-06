690193d9ee0a9299b3000cc7d53c256a81e49a10
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-06-06 13:03:07 +0300

    implemented, kinda

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 def main():
-    # write your code here
-    pass
+    file_name = f"{input('Enter name of the file: ')}.txt"
+    lines: list[str] = []
+    while True:
+        current = input("Enter new line of content: ")
+        if current == "stop":
+            break
+        lines.append(current+"\n")
+    with open(file_name, "w") as handle:
+        handle.writelines(lines)
 
 
 if __name__ == "__main__":

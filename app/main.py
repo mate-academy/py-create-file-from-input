9c5ffeaf2fae5861a34d9fa9e5eabc12a708cf9a
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-06-06 13:04:13 +0300

    fixed stupid flake8 rules

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-def main():
+def main() -> None:
     file_name = f"{input('Enter name of the file: ')}.txt"
     lines: list[str] = []
     while True:
         current = input("Enter new line of content: ")
         if current == "stop":
             break
-        lines.append(current+"\n")
+        lines.append(current + "\n")
     with open(file_name, "w") as handle:
         handle.writelines(lines)
 

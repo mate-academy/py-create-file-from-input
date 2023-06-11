d1ec71c805fbafd1b9a6262bb76446ec2278839f
YOUR_NAME <YOUR_EMAIL>
2023-06-11 19:45:03 +0300

    Create file (first commit)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-def main():
+def main() -> None:
     # write your code here
-    pass
+    name = input("Enter name of the file: ")
+    content_list = []
+
+    while True:
+        content = input("Enter new line of content: ")
+        if content == "stop":
+            break
+        content_list.append(f"{content}\n")
+
+    with open(f"{name}.txt", "w+") as file:
+        for con in content_list:
+            file.write(con)
 
 
 if __name__ == "__main__":

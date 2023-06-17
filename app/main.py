1347080cb69c292f764630cd20907a3774896513
Denys Gordiichuk <dd111320@gmail.com>
2023-06-17 16:15:15 +0300

    f

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    filename = input("Enter name of the file: ")
+    with open(filename + ".txt", "w") as f:
+        while True:
+            inp = input("Enter new line of content: ")
+            if inp == "stop":
+                break
+            else:
+                f.write(inp + "\n")
 
 
 if __name__ == "__main__":

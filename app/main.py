d15b136b64d357cd3547a2ff11465585d4517fdf
Mykhailo Korotia <mr.anderson528491@gmail.com>
2023-07-18 12:56:55 +0300

    solution2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def main() -> None:
     filename = input("Enter name of the file: ")
-    with open(f"{filename}.txt", 'a') as f:
+    with open(f"{filename}.txt", "a") as f:
         while True:
             content = input("Enter new line of content: ")
             if content == "stop":
@@ -10,6 +10,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
-
-

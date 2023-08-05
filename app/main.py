8fdfdb57cc6be834921bc652b8a9f2ca56545af6
Kosta Stepanjuk <stepanjukkosta@qmail.com>
2023-08-06 00:01:45 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    message = ""
+    input_name = input("Enter name of the file: ")
+
+    if input_name:
+        while True:
+            inp_message = input("Enter new line of content: ")
+
+            if inp_message == "stop":
+                break
+            message += f"{inp_message}\n"
+
+    file_name = f"{input_name}.txt"
+    file_discriptor = open(file_name, "w")
+    file_discriptor.write(message)
+    file_discriptor.close()
 
 
 if __name__ == "__main__":

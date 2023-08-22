3c573efad96ce808e1b51f7f5c26c6bbf05946aa
Ruslan <rusipbox@gmail.com>
2023-08-22 20:37:19 +0300

    Test(py-create-file-from-input)

--- app/main.py
+++ app/main.py
@@ -1,7 +1,28 @@
-def main():
-    # write your code here
-    pass
+def create_file_with_content(file_name: str, content: str) -> None:
+    with open(file_name, "w") as file:
+        file.write(content)
+
+
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+    content = ""
+
+    while True:
+        line = input(
+            "Enter new line of content: "
+        )
+        if line == "stop":
+            break
+        content += line + "\n"
+
+    file_name_with_extension = file_name + ".txt"
+    create_file_with_content(file_name_with_extension, content)
+
+    print(f"File name: \"{file_name_with_extension}\"")
+    print("File content:")
+    print(content)
 
 
 if __name__ == "__main__":
     main()
+

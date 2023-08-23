521c3372bbd9768c61205030e12d335371a2086c
ivan <ivan@isba.local>
2023-08-23 14:56:17 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,27 @@
-def main():
-    # write your code here
+class EmptyFilenameError(Exception):
     pass
 
 
+def main() -> None:
+    filename = input("Enter name of the file: ")
+    if not filename:
+        raise EmptyFilenameError("Filename must be not empty")
+    content = ""    
+    while True:
+        input_text = input("Enter new line of content: ")
+        if input_text == "stop":
+            break
+        content += input_text + "\n"
+
+    with open(f"{filename}.txt", "w") as file_:
+        file_.write(content)
+
+
 if __name__ == "__main__":
-    main()
+    while True:
+        try:
+            main()
+        except EmptyFilenameError:
+            continue
+        else:
+            break

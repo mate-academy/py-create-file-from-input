5795ad2643fc0ebd6c616b3c2996048ed4ad97b8
Vasyl Dmytruk <vasyldmytruk104@gmail.com>
2023-08-24 17:13:58 +0300

    create_file_v1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ") + ".txt"
+    content = []
 
+    while True:
+        user_input = input("Enter new line of content: ")
 
-if __name__ == "__main__":
-    main()
+        if user_input == "stop":
+            break
+        content.append(user_input)
+
+    with open(file_name, "w") as file:
+        for line in content:
+            file.write(line + "\n")

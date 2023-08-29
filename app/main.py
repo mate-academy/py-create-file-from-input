6e1237715bd8d8fb6c0c4b6ca9086978b7e6384e
Alla Kuksa <alla.kuksa92@gmail.com>
2023-08-29 20:47:05 +0300

    Solution 2

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 def main() -> None:
 
-    name_of_file = input("Enter name of the file:")
-    with open(name_of_file, "w") as current_file:
-        line = current_file.write(input("Enter new line of content:"))
-        while True:
-            line
-            if line == "stop":
-                break
+    name_of_file = input("Enter name of the file: ") + ".txt"
+    input_messages = []
+
+    while True:
+        line = input("Enter new line of content: ")
+
+        if line.lower() == "stop":
+            break  # Exit the loop when "stop" is entered
+        else:
+            input_messages.append(line)
+
+    with open(name_of_file, "a") as current_file:
+        current_file.write("\n".join(input_messages))
 
 
 if __name__ == "__main__":

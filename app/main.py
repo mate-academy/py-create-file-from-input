fd6af15918c2b2db80a68be7786d3c998fd51438
Daniil Hinzburh <2ilgan1601@gmail.com>
2023-09-05 15:31:55 +0300

    task completed(1)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 def main():
-    # write your code here
-    pass
+    file_name = input("Enter name of the file: ")
+    try:
+        with open(file_name + ".txt", "a") as work_file:
+            stop = False
+            while not stop:
+                line = input("Enter new line of content: ")
+                if line == "stop":
+                    stop = True
+                else:
+                    work_file.write(line + '\n')
+    except FileNotFoundError:
+        print(f"File '{file_name}.txt' not found.")
 
 
 if __name__ == "__main__":

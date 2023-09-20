04f48bf2c5ce801e1bdefbea4d681fb741e04632
Andrew <67094594+MishkaGAMII@users.noreply.github.com>
2023-09-20 21:19:58 +0300

    spac_fix.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 def main() -> None:
     name = input("Enter name of the file: ")
+
     line_list = []
     while True:
         line = input("Enter new line of content: ")
         if line == "stop":
             break
         line_list.append(line)
-    
+
     with open(f"{name}.txt", "w") as file:
         for i in line_list:
             file.write(i + "\n")

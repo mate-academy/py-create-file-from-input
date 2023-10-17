ffb996267e3b74fc5b172d1fb401796dab61388b
Severyn Krok <sevkrok@gmail.com>
2023-10-17 10:02:29 +0300

    Implement py-create-file-from-input

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ") + ".txt"
+    text = ""
+    while True:
+        tmp = input("Enter new line of content: ")
+        if tmp == "stop":
+            break
+        text += tmp + "\n"
+
+    with open(file_name, "w") as f:
+        f.write(text)
 
 
 if __name__ == "__main__":

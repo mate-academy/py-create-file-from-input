40baa0ec6fe0ff4298c657bd7f8223673d93a332
Frezworx <frezworx@gmail.com>
2023-10-23 19:39:22 +0300

    Fix solution

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
-def main():
-    file_name = input("Enter file name: ")
+def main() -> None:
+    file_name = input("Enter name of the file: ") + ".txt"
     str_memory = ""
 
     while True:
-        user_input = input("Enter line of the content: ")
+        user_input = input("Enter new line of content: ")
         if user_input == "stop":
-            with open(file_name, "w") as f:
-                f.write(str_memory)
-            return
-
+            break
         str_memory += user_input + "\n"
+
+    with open(file_name, "w") as f:
+        f.write(str_memory)
 
 
 if __name__ == "__main__":

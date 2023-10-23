bdae901032342291755adfda5170103f9f6acb13
Frezworx <frezworx@gmail.com>
2023-10-23 19:17:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 def main():
-    # write your code here
-    pass
+    file_name = input("Enter file name: ")
+    str_memory = ""
+
+    while True:
+        user_input = input("Enter line of the content: ")
+        if user_input == "stop":
+            with open(file_name, "w") as f:
+                f.write(str_memory)
+            return
+
+        str_memory += user_input + "\n"
 
 
 if __name__ == "__main__":

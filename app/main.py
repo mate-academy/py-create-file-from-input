a9c3e75fef7a169c363a56128ef6705090bb4c47
Borys Kirichenko <boryskirichenko99@gmail.com>
2023-12-07 13:08:32 +0100

    all tests passed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 def main():
-    # write your code here
-    pass
+    f_name = input("Enter name of the file: ") + ".txt"
+    f_contest = ""
+
+    while True:
+        new_line = input("Enter new line of content: ")
+        if new_line.lower() == "stop":
+            break
+        else:
+            f_contest += new_line + "\n"
+
+    with open(f_name, "w") as f:
+        f.write(f_contest)
 
 
 if __name__ == "__main__":

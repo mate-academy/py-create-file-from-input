162d8970feff521544cb4a17267911cab532a4da
Volodymyr Kovalenko <380974261314kv@gmail.com>
2023-12-08 22:52:13 +0200

    created file with content

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ") + ".txt"
+    content = ""
+    while content != "stop" + "\n":
+        with open(file_name, "a") as f:
+            f.write(content)
+            content = input("Enter new line of content: ") + "\n"
 
 
 if __name__ == "__main__":

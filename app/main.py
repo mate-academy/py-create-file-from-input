586ac9925780d4cbbd12f1f4de776464b0416acf
artem.puhach <artem.puhach@cyberpollice.gov.ua>
2023-12-16 12:14:47 +0200

    ready

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    filename = input("Enter name of the file: ")
+    if not filename.endswith(".txt"):
+        filename = filename + ".txt"
+    buffer = []
+    while True:
+        input_line = input("Enter new line of content: ")
+        if input_line == "stop":
+            break
+        buffer.append(input_line)
+    with open(filename, "w") as f:
+        [f.write(line + "\n") for line in buffer]
 
 
 if __name__ == "__main__":

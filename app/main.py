c55d91d73022a833ce497294819eacadb1e2bede
yes <yes@yess-MacBook-Pro.local>
2024-01-17 23:16:33 +0200

    done

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
 def main():
-    # write your code here
-    pass
+    try:
+        file = open(file=f"{input("Enter name of the file: ")}.txt", mode="x")
+
+        while True:
+            content = input("Enter new line of content: ")
+
+            if content:
+                if content == "stop":
+                    file.close()
+                    break
+                else:
+                    file.write(f"{content}\n")
+            else:
+                continue
+
+    except FileExistsError:
+        if input("file name already is exist\ntry again? : (y)") == "y":
+            main()
+        else:
+            return None
 
 
 if __name__ == "__main__":

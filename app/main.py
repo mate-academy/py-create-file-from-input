b34ba94503901d8c70641ca58bbb9431410b81f0
AntonSkyrda <mr.skyrda@gmail.com>
2024-01-24 17:04:12 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 def main():
-    # write your code here
-    pass
+    file_name = input("Enter file name: ")
+    file_text = ""
+    while True:
+        user_text = input("Enter a text to save it to file: ")
+
+        if user_text == "stop":
+            break
+
+        file_text += f"{user_text}\n"
+
+    with open(file_name, "w") as file:
+        file.write(file_text)
 
 
 if __name__ == "__main__":

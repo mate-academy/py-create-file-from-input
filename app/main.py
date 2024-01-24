fca7bb81e37bea997c31ff7e5a6a41ec7adcc05d
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-01-24 15:47:41 +0200

    Sol

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 def main():
-    # write your code here
-    pass
+    name_of_the_file = input("Enter name of the file: ") + ".txt"
+
+    new_line_of_content = input("Enter new line of content: ")
+
+    content_to_write = []
+
+    while new_line_of_content != "stop":
+
+        if new_line_of_content != "stop":
+            content_to_write.append(f"{new_line_of_content}\n")
+
+        new_line_of_content = input("Enter new line of content: ")
+
+    with open(name_of_the_file, "a") as file:
+        for line in content_to_write:
+            file.write(line)
 
 
 if __name__ == "__main__":

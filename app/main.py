b33dda4a9cfc431fa18b3088b18accd1ef1c7206
Serhii Buryk <sergmarten21@gmail.com>
2024-01-24 14:52:50 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,16 @@
 def main():
-    # write your code here
+    file_name = input("Enter name of the file: ")
+    content_box = []
+    while True:
+        content = input("Enter new line of content: ")
+        content_box.append(content)
+
+        if content == "stop":
+            content_box.pop(len(content_box) - 1)
+            with open(f"{file_name}.txt", "w") as file:
+                for element in content_box:
+                    file.write(f"{element}\n")
+            break
     pass
 
 

5ff8196d0afb6ad700788dff5a050657623aa48f
vova-stoliar <vova.stoliar@overdose.digital>
2024-03-21 19:45:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    content: list[str] = []
+    file_name = ""
+    keyword_to_break_loop = "stop"
+
+    while True:
+        if not file_name:
+            file_name = input("Enter name of the file: ")
+
+        entered_content = input("Enter new line of content: ")
+
+        if entered_content == keyword_to_break_loop:
+            break
+
+        content.append(entered_content + "\n")
+
+    with open(f"{file_name}.txt", "w") as file:
+        file.writelines(content)
 
 
 if __name__ == "__main__":

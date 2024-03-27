4230a001b23cfc0d9bfe47a15951baf6cf32555e
Valentin Pozniak <waliker448@gmail.com>
2024-03-27 13:47:15 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def main() -> None:
     name = input("Enter name of the file: ")
-    with open(name, "a") as f:
+    with open(f"{name}.txt", "a") as f:
         while 1:
             info = input("Enter new line of content: ")
             if info == "stop":

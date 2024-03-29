34a19e38509a480ee5b473913f59143cdd714814
Marharyta Marchenko <ritanika12@gmail.com>
2024-03-29 15:22:55 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 def main():
-    # write your code here
-    pass
+    def create_file():
+        file_name = input("Enter name of the title: ")
+        content = []
+        while True:
+            line = input("Enter new line of content: ")
+            if line.lower() == "stop":
+                break
+            content.append(line)
+
+        file_name += ".txt"
+        with open(file_name, "w") as file:
+            for line in content:
+                file.write(line + "\n")
+
+    create_file()
 
 
 if __name__ == "__main__":

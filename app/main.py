01c0e28321db95780c9fa9135aae5716002835f8
Githi54 <chinter.games1@gmail.com>
2024-03-29 23:29:58 +0200

    fixes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def main() -> None:
     file_name = input("Enter name of the file: ") + ".txt"
 
-    with open(file_name, 'a') as file:
+    with open(file_name, "a") as file:
         while True:
             file_content = input("Enter new line of content: ")
             if file_content == "stop":

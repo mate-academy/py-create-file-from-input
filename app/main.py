e4b7bf2f9467b9b750da7dc4f40ddc4b2a4500c0
= <=>
2024-03-29 15:14:28 +0200

    wrote code that take data from terminal and writing to file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    line_list = []
+    filename = input("Enter name of the file: ") + ".txt"
+    while True:
+        line = input("Enter new line of content: ")
+        if line != "stop":
+            line_list.append(line)
+        else:
+            break
+
+    with open(filename, "a") as f:
+        for line in line_list:
+            f.write(f"{line}\n")
 
 
 if __name__ == "__main__":

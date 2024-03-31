3191cd45a487786c28cb0234941d02ecbfb6a62d
Oleksandr Moroz <molecxander@gmail.com>
2024-03-31 20:24:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ") + ".txt"
+    content = []
+    while True:
+        line = input("Enter new line of content: ")
+        if line.lower() == "stop":
+            break
+        content.append(line)
+
+    with open(file_name, "w") as f:
+        for line in content:
+            f.write(line + "\n")
+    print(f"\nFile name: \"{file_name}\"")
+    print("File content:")
+    for line in content:
+        print(f"# {line}")
 
 
 if __name__ == "__main__":

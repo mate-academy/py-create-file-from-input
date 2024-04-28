9ea312562bb20fd0f710ad9514b9589167c08506
Мар'яна Тиха <mariana@tikhaya@gmail.com>
2024-04-28 13:24:43 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
-def main():
-    # write your code here
-    pass
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+    content_lines = []
+    while True:
+        line = input("Enter new line of content: ")
+        if line.lower() == "stop":
+            break
+        content_lines.append(line)
+    with open(f"{file_name}.txt", "w") as file:
+        for line in content_lines:
+            file.write(line + "\n")
+
+    print(
+        f"File '{file_name}.txt' has been created successfully "
+        f"with the provided content."
+    )
 
 
 if __name__ == "__main__":

e57773572f00362de2752de8d7772a65d9a115f9
Igor Kolisnyk <moonbosszzz@gmail.com>
2024-05-28 17:15:44 +0200

    The solution of the task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-def main():
+def main() -> None:
     # write your code here
-    pass
+    with open(f"""{input("Enter name of the file: ")}.txt""", "a") as file:
+        while True:
+            user_input = input("Enter new line of content: ")
+            if user_input == "stop":
+                break
+            file.write(f"{user_input}\n")
 
 
 if __name__ == "__main__":

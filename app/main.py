171e4fe514fec8fb6b79c6ce1bdce5300a3aff32
dsokolovskiy <souldead147@gmail.com>
2024-05-31 11:03:11 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,26 +2,26 @@ from typing import List
 
 
 def create_file() -> None:
-    file_name = input("Enter name of the file: ")
+    file_name = input('Enter name of the file: ')
     lines: List[str] = []
     while True:
-        line = input("Enter new line of content: ")
-        if line.lower() == "stop":
+        line = input('Enter new line of content: ')
+        if line.lower() == 'stop':
             break
         lines.append(line)
-    file_content = "\n".join(lines)
-    file_name += ".txt"
-    with open(file_name, "w") as file:
+    file_content = '\n'.join(lines)
+    file_name += '.txt'
+    with open(file_name, 'w') as file:
         file.write(file_content)
-    print(f"# File name: \"{file_name}\"")
-    print("# File content:")
+    print(f'# File name: "{file_name}"')
+    print('# File content:')
     for line in lines:
-        print("# " + line)
+        print('# ' + line)
 
 
 def main() -> None:
     create_file()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()

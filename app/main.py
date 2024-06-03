efb4c0154b914317c8a3926db73dbb90f51b6d7f
jpdreamthug <lentorilp@gmail.com>
2024-06-03 14:38:01 +0300

    fix input

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def main() -> None:
-    file_name = input("Enter name of the file: ") + ".txt"
-    with open(file_name, "w") as f:
+    file_name = input("Enter name of the file: ")
+    with open(f"{file_name}.txt", "w") as f:
         while True:
-            new_line = input("Enter the new line of content: ")
+            new_line = input("Enter new line of content: ")
             if new_line.lower() == "stop":
                 break
             f.write(new_line + "\n")

eb427c7871bf11343027535fa20840cbf73e54fe
Alex93Troy <troianovskiy93alex@gmail.com>
2024-06-16 17:51:30 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 def main() -> None:
-    file_name = input() + ".txt"
+    file_name = input("Enter name of the file: ") + ".txt"
     content = []
 
     while True:
-        line = input()
+        line = input("Enter new line of content: ")
         if line.lower() == "stop":
             break
         content.append(line)
 
-    with open(file_name, "w", encoding="utf-8") as file:
+    with open(file_name, "w") as file:
         for line in content:
             file.write(line + "\n")
 

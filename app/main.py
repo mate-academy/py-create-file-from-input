e3ce8dcd53a122e66e104453c213cbf588d8ff38
Andriy Zhuk <andri195595@gmail.com>
2024-06-24 20:05:31 +0300

    +

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    name_file = input("Enter name of the file: ")
+    user_text = []
+
+    while True:
+        text_temp = input("Enter new line of content: ")
+        if text_temp == "stop":
+            break
+        user_text.append(text_temp)
+
+    with open(f"{name_file}.txt", "w") as user_file:
+        for line in user_text:
+            user_file.write(line + "\n")
 
 
 if __name__ == "__main__":

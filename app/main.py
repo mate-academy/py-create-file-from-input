72c825a5899bc252a365b2d8263cf47e9113b63e
Olexandr Bastov <iamlucky1990@gmail.com>
2024-07-30 23:53:05 +0300

    ".txt" added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def main() -> None:
     file_name = input("Enter name of the file: ")
-    with open(file_name, "a") as file:
+    with open(file_name + ".txt", "a") as file:
         while True:
             text = input("Enter new line of content: ")
             if text == "stop":

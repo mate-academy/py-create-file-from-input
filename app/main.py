963a4c49121c774185d3fd851d38d456130a4e5d
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-05 20:36:50 +0300

    first commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,36 @@
+import os
+
+
+def get_file_name():
+    while True:
+        file_name = input("Enter name of the file: ")
+        full_file_name = file_name + ".txt"
+
+        if os.path.exists(full_file_name):
+            print(f"Файл '{full_file_name}' уже существует.")
+            overwrite = input("Хотите перезаписать файл? (да/нет): ").strip().lower()
+            if overwrite == "да":
+                return full_file_name
+        else:
+            return full_file_name
+
+
 def main():
-    # write your code here
-    pass
+    full_file_name = get_file_name()
+
+    content = []
+
+    while True:
+        line = input("Enter new line of content: ")
+        if line.lower() == "stop":
+            break
+        content.append(line)
+
+    with open(full_file_name, 'w') as file:
+        for line in content:
+            file.write(line + '\n')
+
+    print(f"Файл '{full_file_name}' был успешно создан и заполнен.")
 
 
 if __name__ == "__main__":

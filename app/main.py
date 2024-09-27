f1aeb54709e7ad07e5036c0504b18848c401ec5b
Lev <lev2099@gmail.com>
2024-09-27 18:34:29 +0300

    input to "file_name".txt

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 def main():
-    # write your code here
-    pass
+    name_file = input("Enter name of the file: ")
+    with open(f"{name_file}.txt", "w") as f:
+        while True:
+            file_contetn = input("Enter new line of content: ")
+            if file_contetn == "stop":
+                break
+            else:
+                f.write(f"{file_contetn}\n")
+    with open(f"{name_file}.txt", "r") as file:
+        print(file.read())
 
 
 if __name__ == "__main__":

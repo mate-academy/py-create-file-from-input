9610c2cc26c9cc93676303467c71a4dbe4b1274d
Anton <1r0nt0ny1970@gmail.com>
2024-10-31 18:17:02 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    """Prompts user for a file name and content, then creates the file."""
+    file_name = input("Enter name of the file: ").strip() + ".txt"
+
+    content = []
+    while True:
+        line = input("Enter new line of content: ")
+        if line.lower() == "stop":
+            break
+        content.append(line)
+
+    with open(file_name, "w") as file:
+        file.write("\n".join(content))
+
+    print(f'File "{file_name}" has been created with the following content: ')
+    for line in content:
+        print(line)
 
 
 if __name__ == "__main__":

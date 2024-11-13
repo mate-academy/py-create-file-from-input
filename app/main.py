7c6de0d7626c500fa84361be35909631f278ec26
ValeriyIvanovich <98410772+ValeriyIvanovich@users.noreply.github.com>
2024-11-13 20:46:14 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def main() -> None:
     file_name = input("Enter name of the file: ")
     with open(f"{file_name}.txt", "w") as f:
-         while True:
+        while True:
             line_input = input("Enter new line of content: ")
             if line_input == "stop":
                 break

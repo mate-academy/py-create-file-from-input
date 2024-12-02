ac174c911b25357d72fc464533f393547f2e0574
ArtemKhymenko <artem.khymenko@gmail.com>
2024-12-02 11:30:58 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    dialogue = input("Enter name of the file: ")
+    lines = []
+    while True:
+        line = input("Enter new line of content: ")
+        if line == "stop":
+            break
+        lines.append(line)
+    with open(f"{dialogue}.txt", "w") as file:
+        for line in lines:
+            file.write(line + "\n")
 
 
 if __name__ == "__main__":

35a566659f5b698d26e2df2f12bd73cde2a265bc
artem <krenky.spenky@gmail.com>
2024-12-14 13:45:16 +0200

    'Sol'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ") + ".txt"
+
+    content_lines = []
+
+    while True:
+        new_line = input("Enter new line of content: ")
+
+        if new_line.lower() == "stop":
+            break
+
+        content_lines.append(new_line)
+
+    with open(file_name, "w") as file:
+        for line in content_lines:
+            file.write(line + "\n")
 
 
 if __name__ == "__main__":

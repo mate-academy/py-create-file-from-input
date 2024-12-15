5a9c231849e3c1ae0515d466993aa37a34ddb043
Koziakov Hlib <hlibs.work@gmail.com>
2024-12-15 14:25:02 +0200

    Integration possibility to write in new file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 def main():
-    # write your code here
-    pass
+    name = input("Enter name of the file: ")
+
+    new_line = input("Enter new line of content: ")
+    with open(name + ".txt", "a") as input_file:
+        while new_line != "stop":
+            input_file.write(new_line + "\n")
+            new_line = input("Enter new line of content: ")
 
 
 if __name__ == "__main__":

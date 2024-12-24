23e5f6e6a905fc02041e8067e8a182b4b68596cf
Liza Shulika <liza.shulika2009@gmail.com>
2024-12-24 15:45:25 +0200

    from_file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 def main():
     # write your code here
-    pass
+    name = input("Enter name of the file: ") + ".txt"
+    lines = []
+    while True:
+        line = input("Enter new line of content: ")
+        if line == "stop":
+            break
+        lines.append(line)
+    with open(f"{name}", "w") as f:
+        f.write(f"File name: {name}\n")
+        f.write(f"File content:\n")
+        f.write("\n".join(lines))
 
 
 if __name__ == "__main__":

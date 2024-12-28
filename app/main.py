1474973a8cdf5913ba4a8782357c1ec6e538c3b5
JulianskyMe <juliakamynskaya@gmail.com>
2024-12-28 16:06:00 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,31 @@
+import os
+
+
 def main():
-    # write your code here
-    pass
+    file_name = input("Enter the name of the file without extensions. Just the name:  ")
+    check_type(file_name)
+    
+    with open(file_name + ".txt", "a") as file:
+        while True:
+            next_line = input("Next line, please. Type in 'stop' to quit input:  ")
+            if next_line.lower() == "stop":
+                break
+            else: 
+                file.write(next_line + "\n")
+    
+    print(f"File name: {file_name}.txt \nFile content:\n {read_lines(file_name + '.txt')}")
+
+
+def check_type(file_name: str) -> None:
+    _, extension = os.path.splitext(file_name)
+    
+    if extension and extension != '.txt':
+        raise ValueError("Error: The file must be a .txt file")
+
+
+def read_lines(file_name: str) -> str:
+    with open(file_name, "r") as file:
+        return file.read()
 
 
 if __name__ == "__main__":

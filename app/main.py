3853f23d0ea186c701af660bab633753990b2f1c
Yasha Lapin <yasha.lapin@gmail.com>
2025-01-06 16:55:34 +0200

    CreateFileFromInput

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-def main():
-    # write your code here
-    pass
+import os
+print(f"Current directory: {os.getcwd()}")
+
+
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+    print(f"Creating file: {file_name}.txt")
+    with open(file_name + ".txt", "a") as file:
+        while True:
+            line = input("Enter new line of content: ")
+            if line == "stop":
+                break
+            file.write(line + "\n")
 
 
 if __name__ == "__main__":

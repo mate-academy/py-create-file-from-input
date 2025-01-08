6143fa9b2b54804f18233b1ad42d7713e87faecd
Miwacer <il>
2025-01-08 23:54:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
+import os
+print(f"Current directory: {os.getcwd()}")
+
 def main():
-    # write your code here
-    pass
+    file_name = input("Enter name of the file: ")
+    print(f"Creating file: {file_name}.txt")
+    with open(file_name + ".txt", "a") as file:
+        while True:
+            line = input("Enter new line of content: ")
+            if line == "stop":
+                break
+            file.write(line + "\n")
 
 
 if __name__ == "__main__":

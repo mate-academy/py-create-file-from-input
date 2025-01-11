3ce4f1c7c71105ad1e91d5282ffe78576dc51bb9
Sergii Hrytchyn <ichbinjagerrr@gmail.com>
2025-01-11 21:22:02 +0200

    implement function "main"

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+    with open(file_name + ".txt", "w", newline="") as file:
+        new_line = ""
+        while new_line != "stop":
+            new_line = input("Enter new line of content: ")
+            if new_line != "stop":
+                file.write(new_line + "\n")
 
 
 if __name__ == "__main__":

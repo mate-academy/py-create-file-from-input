dc5447b5fb8740e78f5804d9c44a53d7e0c5200e
Andrii Sokolan <your@mail.com>
2025-01-11 22:49:50 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ")  # Виправлено текст введення
+    lines = []  # Список для збереження рядків
+    while True:
+        new_line = input("Enter new line of content: ")
+        if new_line.lower() == "stop":  # Припинення введення
+            break
+        lines.append(new_line)  # Додаємо рядок до списку
+
+    # Запис у файл
+    with open(f"{file_name}.txt", "w") as f:
+        f.write("\n".join(lines))
 
 
 if __name__ == "__main__":

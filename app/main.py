812c50624b8f75a2da7704325e50921b4a8c745e
Fedir <fedor.agureev@gmail.com>
2025-01-18 19:49:36 +0200

    fixes

--- app/main.py
+++ app/main.py
@@ -1,18 +1,23 @@
 def main() -> None:
-    file_name = input("Enter file name: ")
+    file_name = input("Enter name of the file: ").strip()
 
     if not file_name.endswith(".txt"):
         file_name += ".txt"
+
     content_lines = []
 
     while True:
-        line = input("Enter next line: ")
+        line = input("Enter new line of content: ").strip()
+
         if line.lower() == "stop":
             break
+
         content_lines.append(line)
 
-    with open(file_name, "w") as file:
+    with open(file_name, "w", encoding="utf-8") as file:
         file.write("\n".join(content_lines))
+
+    print(f"Файл '{file_name}' успішно створено.")
 
 
 if __name__ == "__main__":

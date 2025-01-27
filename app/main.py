400cc3744b23742a794da69a36fbe8000cf809a3
Volodymir Shlikhta <shlikhta77@gmail.com>
2025-01-27 21:13:24 +0200

    Add functionality to create text file from user input

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 def main():
-    # write your code here
-    pass
+    file_name = input("Enter name of the file: ")
+
+    lines_of_content = []
+
+    while True:
+        line = input("Enter new line of content: ")
+        if line.lower() == "stop":
+            break
+        lines_of_content.append(line)
+
+    file_name_with_extension = file_name + ".txt"
+
+    with open(file_name_with_extension, "w") as file:
+        for line in lines_of_content:
+            file.write(line + "\n")
+
+    print(f"File '{file_name_with_extension}' created successfully.")
 
 
 if __name__ == "__main__":

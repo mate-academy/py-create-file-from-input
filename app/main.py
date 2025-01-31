79578f763562d215db6b2f41d212d690e6695100
bc2024-4 <bzabrodskij20@gmail.com>
2025-01-31 18:21:02 +0200

    add program

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 def main():
-    # write your code here
-    pass
+    name = input("Enter name of the file:")
+    with open((f"{name}.txt"), "a") as file:
+        while True:
+            text = input("Enter new line of content:")
+            if text == "stop":
+                break
+            file.write(text)
 
 
 if __name__ == "__main__":

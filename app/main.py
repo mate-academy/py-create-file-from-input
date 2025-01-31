074c18b5d7cc4ee8f6c5e4ba3fc4c9efeabbbcd1
bc2024-4 <bzabrodskij20@gmail.com>
2025-01-31 23:00:09 +0200

    fix2

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 def main() -> None:
-    name = input("Enter name of the file:")
-    with open((f"{name}.txt"), "a") as file:
+    name = input("Enter name of the file: ")
+    with open(f"{name}.txt", "a") as file:
         while True:
-            text = input("Enter new line of content:")
+            text = input("Enter new line of content: ")
             if text == "stop":
                 break
-            file.write(text)
+            file.write(f"{text}\n")  # Додаємо рядок правильно
 
 
 if __name__ == "__main__":

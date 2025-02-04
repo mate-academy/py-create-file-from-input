9f7937ba39b67d807fc454e3b943e72422202252
Odin-max <meh84741@gmail.com>
2025-02-04 19:58:38 +0200

    1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 def main():
-    # write your code here
-    pass
+    file_name = input("Enter name of the file: ") + ".txt"
+    content_lines = []
 
+    while True:
+        content = input("Enter new line of content: ")
+        if content.lower() == "stop":
+            break
+        content_lines.append(content)
 
+    with open(file_name, "w") as file:
+        file.write("\n".join(content_lines))
+        
 if __name__ == "__main__":
     main()

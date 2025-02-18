87922a8e2048d1afc012271e1db8ab6402ac2e6c
Your Name <stas101203rakitin@gmail.com>
2025-02-18 13:55:47 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 def main() -> None:
     file_name = input("Enter name of the file: ")
-    text_file = open(f"{file_name}", "w")
-    text_file.write(f"File name: \"{file_name}.txt\"\nFile content: \n")
-    text_file.close()
+    file_name = f"{file_name}.txt"
+
+    content_lines = []
     while True:
-        text = input("Enter new line of content: ")
-        if text == "stop":
+        line = input("Enter new line of content: ")
+        if line.lower() == "stop":
             break
-        else:
-            with open(f"{file_name}", "a") as text_file:
-                text_file.write(f"{text}\n")
+        content_lines.append(line)
+
+    with open(file_name, "w", encoding="utf-8") as file:
+        file.write("\n".join(content_lines))
 
 
 if __name__ == "__main__":

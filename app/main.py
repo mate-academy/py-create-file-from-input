256885cb5841a3bfc164d04ed8f1db116b470220
Taras Shevchyk <tarashevchik27@gmail.com>
2025-02-24 22:31:05 +0200

    rename func

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
-def create_text_file():
+def main() -> None:
     file_name = input("Enter name of the file: ")
     file_name = file_name.strip() + ".txt"
-    
+
     content_lines = []
     while True:
         line = input("Enter new line of content: ")
         if line.lower() == "stop":
             break
         content_lines.append(line)
-    
+
     with open(file_name, "w") as file:
         file.write("\n".join(content_lines))
-    
-    print(f"File '{file_name}' created successfully!")
+
+    print(f'File "{file_name}" has been created successfully.')
+
 
 if __name__ == "__main__":
-    create_text_file()
+    main()

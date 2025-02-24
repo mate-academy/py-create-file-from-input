9f51bc1102f426600dae42d18c35dc3746fa7c9f
HP <ngayka@gmail.com>
2025-02-24 14:07:14 +0200

    Have some problem with pytest. need a help and advice

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+    file1 = open(file_name, "w")
+    file1.close()
+
+    while True:
+        text_to_save = input("Enter new line of content: ")
+        if text_to_save == "stop":
+            break
+
+        with open(file_name, 'a') as file:
+            file.write(text_to_save + "\n")
 
 
 if __name__ == "__main__":

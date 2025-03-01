14ffb1693c0d07fbca6fcbec1865898f87884907
Евгений Наволоков <goustg62@gmail.com>
2025-03-01 11:28:22 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 def main():
-    # write your code here
-    pass
-
+    file_name = input("Enter name of the file: ")
+    cmd_list = list()
+    while True:
+        cmd = input("Enter new line of content: ")
+        if cmd.lower().strip() == "stop":
+            break
+        cmd_list.append(cmd)
+        
+    with open(f"{file_name}.txt", "a") as fn:
+        for i in cmd_list:
+            fn.write(f"{i}\n")
 
 if __name__ == "__main__":
     main()

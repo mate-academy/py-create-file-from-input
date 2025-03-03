8eab50562bcd39a3ee23107b9a92bfa420893f56
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-03-03 17:17:39 +0200

    Solution(v3)

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 def main() -> None:
     file_name = input("Enter name of the file: ") + ".txt"
-    list = []
+    list_ = []
     while True:
         for_write = input("Enter new line of content: ")
         if for_write == "stop":
             break
-        list.append(for_write)
+        list_.append(for_write)
 
     with open(file_name, "a") as file:
-        for char in list:
+        for char in list_:
             file.write(char + "\n")
 
 

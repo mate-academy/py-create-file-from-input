7e06844eef996f20c4a6f0100512085e329f4a2b
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-03-03 17:15:59 +0200

    Solution(v2)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 def main() -> None:
     file_name = input("Enter name of the file: ") + ".txt"
+    list = []
+    while True:
+        for_write = input("Enter new line of content: ")
+        if for_write == "stop":
+            break
+        list.append(for_write)
 
-    with open(file_name, "w") as f:
-        while True:
-            line = input("Enter next line of the content: ")
-            if line.lower() == "stop":
-                break
-            f.write(line + "\n")
+    with open(file_name, "a") as file:
+        for char in list:
+            file.write(char + "\n")
 
 
 if __name__ == "__main__":

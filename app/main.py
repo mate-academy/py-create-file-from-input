5fe4c83806ddc37554a837b65af9c53a1d4dd3f6
Maksym Omelkov <omelkov85@gmail.com>
2025-03-04 00:12:36 +0200

    Create a file from input (1.0)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
-def main():
-    # write your code here
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+    with open(f"{file_name}.txt", "a") as file_stream:
+        stop = False
+        while stop is False:
+            new_line = input("Enter new line of content: ")
+            if new_line != "stop":
+                file_stream.write(new_line + "\n")
+            else:
+                stop = True
     pass
 
 

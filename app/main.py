5fcf3ea6e8b2a39a7371408da89a6f0d5ac2d285
Maxim Dovganiuk <maxim20029@gmail.com>
2025-03-12 01:18:00 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
-def main():
+def main() -> None:
     name = input("Enter name of the file: ")
     with open(f"{name}.txt", "a") as file:
         line = ""
         while line != "stop":
             line = input("Input content, or raise 'stop'.")
             file.write(f"{line}\n")
-
 
 
 if __name__ == "__main__":

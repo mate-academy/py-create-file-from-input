3f90fe67f325b297402e011d3a252da0f28f5bf0
Dmytro Lisovyi <kenedy11@ukr.net>
2025-03-18 21:18:35 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,14 @@
-def main():
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+    string = ""
+    string_full = []
+    while string != "stop":
+        string = input("Enter new line of content: ")
+        string_full.append(string)
+    string_full.remove("stop")
+
+    with open(f"{file_name}.txt", "w") as f:
+        f.write("\n".join(string_full))
     # write your code here
     pass
 

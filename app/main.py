da3896c06ff92d56772e843941cb04dea401d956
Andrii Martyniuk <andrijmartunjyk@gmail.com>
2025-03-27 16:02:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def main() -> None:
-    file_name = f"{input("Enter name of the file: ")}.txt"
+    file_name = input("Enter name of the file: ") + ".txt"
 
     with open(file_name, "a") as f:
         while True:
-            write_line = f"{input("Enter new line of content: ")}\n"
+            write_line = input("Enter new line of content: ") + "\n"
             if str(write_line.lower().strip()) == "stop":
                 break
             f.write(write_line)

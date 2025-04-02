4c04771c2d737727701973d173828a07b960cf47
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-02 19:37:39 +0300

    main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def main():
+def main() -> None:
     file_name = input("Enter name of the file: ")
     file_content = []
 
@@ -11,6 +11,7 @@ def main():
     with open(f"{file_name}.txt", "w") as file:
         for line in file_content:
             file.write(line + "\n")
+
 
 if __name__ == "__main__":
     main()

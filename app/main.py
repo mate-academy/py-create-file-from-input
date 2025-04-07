1dcdd4d015d3b4543f9da079351a43c42624c1aa
Artem Riabchynskyi <ryabchinskyam@gmail.com>
2025-04-07 22:32:34 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+    app_atributes = []
+    while True:
+        enter_line = input("Enter new line of content: ")
+        if enter_line.lower() == "stop":
+            break
+        app_atributes.append(enter_line)
+
+    full_file_name = file_name + ".txt"
+    try:
+        with open(full_file_name, "w") as file:
+            for item in app_atributes:
+                file.write(item + "\n")
+        print(f"Файл '{full_file_name}' успішно створено.")
+    except Exception as e:
+        print(f"Виникла помилка при створенні файлу: {e}")
 
 
 if __name__ == "__main__":

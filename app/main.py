e3b4211483458f398c77818e0c0a48a2fa972849
Vitalik <lg.mobile0808@gmail.com>
2025-04-08 16:18:38 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def main() -> None:
-    file_name = input("Enter the name of file: ") + ".txt"
+    file_name = input("Enter name of the file: ") + ".txt"
 
     with open("new_file.txt", "a") as file:
-        #print(f"File name: {file_name}.txt")
-        #print("File content:")
+        file.write(f"File name: {file_name}")
+        file.write("File content:")
         while True:
             baka = input("Enter new line of content: ")
             if baka.lower() == "stop":

397ccceb125ad1e6b622dfe141c2a4a94c6aba4d
Vitalii Prushak <v.prushak@gmail.com>
2025-04-17 19:30:27 +0300

    Solution: attempt #1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,28 @@
 def main():
-    # write your code here
-    pass
+    body = ""
+    file_name = ""
+    while not file_name:
+        file_name = input("Please enter the file name")
 
+    if not file_name.endswith(".txt"):
+        file_name += ".txt"
+
+    print('Now you will have to fill the file.\nPrint "stop" to finish.')
+
+    while True:
+        line = input("Enter data to file")
+        if line.lower() == "stop":
+            break
+        body += line + "\n"
+
+    print("Saving the file")
+
+    with open(file_name, "w") as target:
+        target.writelines(body)
+
+    print(f"File was saved as {file_name} in working directory.")
+
+    return
 
 if __name__ == "__main__":
     main()

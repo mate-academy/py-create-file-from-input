4f6fc9c77b8b0565ee90a50b4cb905739b69ad5b
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-22 03:12:59 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def main():
+def main() -> None:
     file_name = input("Enter name of the file: ") + ".txt"  # Ensure `.txt` extension
     with open(file_name, "a") as file:
         while True:
@@ -8,6 +8,7 @@ def main():
             file.write(input_content + "\n")
 
     print(f'File "{file_name}" created successfully.')
+
 
 if __name__ == "__main__":
     main()

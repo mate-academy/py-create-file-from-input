803f72248fe7768ffe44e54656264a3af71fa6d1
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-22 03:04:32 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-def main():
-    # write your code here
-    pass
-
+def main() -> None:
+    file_name = input("Enter file name: ")
+    file = open(f"{file_name}", "a")
+    input_content = ""
+    while input_content != "stop":
+        input_content = input("Enter content: ")
+        file.write(input_content)
 
 if __name__ == "__main__":
     main()

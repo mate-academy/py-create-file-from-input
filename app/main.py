68b97d23cad47c19580311fab394220b990b5048
Andrii Omelchenko <andreyomem@gmail.com>
2025-05-19 15:41:13 +0300

    implement file output

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+    input_continue = True
+    file_output = open(f"{file_name}.txt", "a")
+    while input_continue:
+        file_line = input("Enter new line of content: ")
+        if file_line != "stop":
+            file_output.write(f"{file_line}\n")
+        if file_line == "stop":
+            input_continue = False
 
 
 if __name__ == "__main__":

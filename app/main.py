a01b8c3bf90fb8388bb147f1520c0cc11c085a82
Denis <godlike.kipish@gmail.com>
2025-05-31 18:13:33 +0300

    From second time?

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def main():
+def main() -> None:
     file_name = input("Enter name of the file: ")
     with open(file_name + ".txt", "a") as f:
         while True:
@@ -6,7 +6,6 @@ def main():
             if new_content == "stop":
                 break
             f.write(new_content + "\n")
-
 
 
 if __name__ == "__main__":

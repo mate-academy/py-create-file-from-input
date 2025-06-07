fa71a6720d5e61ce5a5586dd7bd03448c781de94
Dmitrii Kozoriz <diluck2002@gmail.com>
2025-06-07 22:10:57 +0300

    Solution_2

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-def main():
+def main() -> None:
     append_list = []
     name = input("Enter name of the file:")
     while True:
-        x = input("Enter new line of content:")
-        if x.lower() == "stop":
+        content = input("Enter new line of content:")
+        if content.lower() == "stop":
             break
-        append_list.append(x)
+        append_list.append(content)
     with open(f"{name}.txt", "a") as f:
         for i in append_list:
             f.write(f"{i}\n")

adaf926064d4d2e1dcb525828b70e03b05cdf511
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-06-09 21:56:36 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,31 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    filename = input("Enter name of the file: ")
+    if not filename.endswith(".txt"):
+        filename += ".txt"
+
+    content = []
+    while True:
+        line = input("Enter new line of content: ")
+        if line.lower() == "stop":
+            break
+        content.append(line)
+
+    # Записуємо вміст у файл
+    with open(filename, "w") as file:
+        file.write("\n".join(content))
+
+    # Читаємо файл і шукаємо слова на w/W
+    w_words = []
+    with open(filename, "r") as file:
+        text = file.read()
+        words = text.split()
+
+        for word in words:
+            if word.lower().startswith("w"):
+                w_words.append(word.lower())
+
+    w_words.sort()
+    print(w_words)
 
 
 if __name__ == "__main__":

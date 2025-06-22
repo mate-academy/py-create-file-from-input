55cc4067e24d4c98b5faebe266fc082f02bd112f
roman-omliukh <romanomeliukh.t@gmail.com>
2025-06-22 13:07:53 +0300

    solutio

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def main() -> None:
-    file_name = input("write name of the file_result")
+    file_name = input("Enter name of the file: ")
     file_name = file_name + ".txt"
     file_result = open(file_name, "w")
     while True:
-        user_input = input("write line")
+        user_input = input("Enter new line of content: ")
         if user_input.strip().lower() == "stop":
             file_result.close()
             break

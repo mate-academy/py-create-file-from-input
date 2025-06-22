2e1544c89114371656001e304bbb1569ffedf49e
roman-omliukh <romanomeliukh.t@gmail.com>
2025-06-22 13:03:55 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("write name of the file_result")
+    file_name = file_name + ".txt"
+    file_result = open(file_name, "w")
+    while True:
+        user_input = input()
+        if user_input.strip().lower() == "stop":
+            file_result.close()
+            break
+        file_result.write(user_input)
+        file_result.write("\n")
+    file_result.close()
 
 
 if __name__ == "__main__":

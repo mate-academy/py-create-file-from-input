ec054abe1695b7aaa7d67fcfff4bf871eadee6ed
Viktor <viktor.maksymiv.23@pnu.edu.ua>
2025-07-03 01:49:12 +0300

    apply flake8 rules

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def main():
+def main() -> None:
     file_name = input("Enter name of the file: ")
     file_content = []
 
@@ -6,11 +6,11 @@ def main():
     while line != "stop":
         file_content.append(f"{line}\n")
         line = input("Enter new line of content: ")
-    
+
     with open(f"{file_name}.txt", "w") as fin:
         for line in file_content:
             fin.write(line)
-    
+
 
 if __name__ == "__main__":
     main()

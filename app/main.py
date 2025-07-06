038689440391965fe7660d53367a146293d86a9b
godfri <godfri@kali>
2025-07-06 20:46:08 +0300

    Add solution for read_from_file task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 def main():
-    # write your code here
-    pass
+    filename = input("Enter name of the file: ")
+
+    content_lines = []
+
+    while True:
+        line = input("Enter new line of content: ")
+        if line == "stop":
+            break
+        content_lines.append(line)
+
+    file_path = f"{filename}.txt"
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        for line in content_lines:
+            file.write(line + '\n')
 
 
 if __name__ == "__main__":

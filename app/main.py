78972e53464ccdfc55e9d0400b1d4fa6fc1e83ae
Andrii Novikov <forytube2023@gmail.com>
2025-07-08 13:42:14 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 def main():
-    # write your code here
-    pass
+    filename = input("Enter name of the file: ").strip()
+    lines = []
+
+    while True:
+        line = input("Enter new line of content: ")
+        if line.lower() == "stop":
+            break
+        lines.append(line)
+
+    full_filename = f"{filename}.txt"
+    try:
+        with open(full_filename, "w", encoding="utf-8") as file:
+            for line in lines:
+                file.write(line + "\n")
+        print(f'File "{full_filename}" created successfully.')
+    except Exception as e:
+        print(f"An error occurred while writing the file: {e}")
 
 
 if __name__ == "__main__":

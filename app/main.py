1ef3843496b097ca6e1aea94a47d29c6ec12c594
Glego <glego200323@gmail.com>
2025-07-08 22:40:23 +0300

    Solution fix "" and ''

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def main() -> None:
-    file_write = open(f"{input("Enter name of the file: ")}.txt", "w")
+    file_write = open(f'{input("Enter name of the file: ")}.txt', "w")
     while True:
         line = input("Enter new line of content: ")
         if line == "stop":

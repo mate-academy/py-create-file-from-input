d303ea85a4d27de5a8337e67a26a21ac615156b2
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2025-07-27 14:41:29 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-def main():
-    # write your code here
-    pass
+STOP_COMMAND = "stop"
+
+
+def main() -> None:
+    file_content: list[str] = []
+    file_name: str = input("Enter name of the file: ")
+
+    while True:
+        content_input: str = input("Enter new line of content: ")
+
+        if content_input == STOP_COMMAND:
+            break
+
+        file_content.append(f"{content_input}\n")
+
+    with open(f"{file_name}.txt", "a") as file:
+        for line in file_content:
+            file.write(line)
 
 
 if __name__ == "__main__":

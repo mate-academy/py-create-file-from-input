6e03393be26873d77bb586bdf98504419ab0c242
Anastasiia Chikrizova <78729920+Anastasiia-Chikrizova@users.noreply.github.com>
2025-08-04 13:49:22 +0000

    f

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
-def main():
+def main() -> None:
     file_name = input("Enter name of the file: ").strip()
     full_name = f"{file_name}.txt"
 
     lines = []
     while True:
         line = input("Enter new line of content: ")
-        if line.lower() == 'stop':
+        if line.lower() == "stop":
             break
         lines.append(line)
 
-    with open(full_name, 'w', encoding='utf-8') as f:
-        f.write('\n'.join(lines))
+    with open(full_name, "w", encoding="utf-8") as f:
+        f.write("\n".join(lines))
 
     print(f"File '{full_name}' created successfully.")
 

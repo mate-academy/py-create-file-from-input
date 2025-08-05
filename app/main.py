f631a90540b8e3d40113c9d16014473fb2172d0c
Maksym Pakula <gargantadoto@gmail.com>
2025-08-05 17:17:50 +0300

    fixed some issues

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 def main() -> None:
     filename = input("Enter name of the file: ")
-    file = open(filename + ".txt", "a")
+    working_file = open(filename + ".txt", "a")
     while True:
         new_line = input("Enter new line of content: ")
         if new_line == "stop":
             break
-        file.write(new_line + "\n")
-    file.close()
+        working_file.write(new_line + "\n")
+    working_file.close()
+
 
 if __name__ == "__main__":
     main()

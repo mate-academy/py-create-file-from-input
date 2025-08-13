ce3a16e4f82a17de3f10cfc8f8ceb1206200bfe9
Samuel Yudi <toyotaa038@gmail.com>
2025-08-13 17:18:26 -0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,16 @@
 def main() -> None:
-    """Solicita ao usuário um nome de arquivo e conteúdo, criando o arquivo .txt."""
     file_name = input("Enter name of the file: ").strip()
     if not file_name.endswith(".txt"):
         file_name += ".txt"
 
     lines = []
 
-    # Loop para receber conteúdo até digitar 'stop'
     while True:
         line = input("Enter new line of content: ")
         if line.strip().lower() == "stop":
             break
         lines.append(line)
 
-    # Cria o arquivo e escreve o conteúdo
     with open(file_name, "w", encoding="utf-8") as f:
         for line in lines:
             f.write(line + "\n")

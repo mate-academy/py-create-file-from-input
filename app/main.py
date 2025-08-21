c5d552177ea184691c19b294b55b261b4b99478d
Serhii Chumak <s4umak@gmail.com>
2025-08-21 19:42:58 +0300

    Solution 7

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def main():
+def main() -> None:
     while True:
         file_name = input("Enter name of the file: ")
         if file_name.strip():
@@ -6,12 +6,13 @@ def main():
     content = []
     while True:
         line = input("Enter new line of content: ")
-        if line.lower() == 'stop':
+        if line.lower() == "stop":
             break
         content.append(line)
     full_file_name = f"{file_name}.txt"
-    with open(full_file_name, 'w', encoding='utf-8') as f:
-            f.write('\n'.join(content))
+    with open(full_file_name, "w", encoding="utf-8") as f:
+        f.write("\n".join(content))
+
 
 if __name__ == "__main__":
     main()

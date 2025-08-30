624ed2fc170c993fe892aa65c923217b449ff928
vasylhladyshev404 <vasyl@nextgear.nl>
2025-08-30 09:54:05 +0200

    1

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 def main() -> None:
     file_name = input("Enter name of the file: ") + ".txt"
 
-    f = open(file_name, "a", encoding="utf-8")
+    file = open(file_name, "a", encoding="utf-8")
 
     while True:
         new_line = input("Enter new line of content: ")
         if new_line == "stop":
             break
-        f.write(new_line + "\n")
+        file.write(new_line + "\n")
 
-    f.close()
+    file.close()
 
 
 if __name__ == "__main__":

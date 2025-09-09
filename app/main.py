87cfb39d8c14f71284bed12b7b82fe2ef6aafbda
Gena <gena.anzin@ukr.net>
2025-09-09 15:21:00 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def main():
+def main() -> None:
     name_of_file = input("Enter file name: ")
     new_text = []
     while True:
@@ -7,10 +7,11 @@ def main():
             break
         else:
             new_text.append(new_line)
-    with open(f"{name_of_file}.txt", 'w') as text_file:
+    with open(f"{name_of_file}.txt", "w") as text_file:
         for text in new_text:
             text_file.write(f"{text}\n")
     pass
+
 
 if __name__ == "__main__":
     main()

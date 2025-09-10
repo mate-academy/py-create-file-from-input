7427a0d36efbc80988190875196ffada1cdf19a6
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-09-10 04:17:58 -0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ def main() -> None:
     file_name = input("Enter name of the file: ")
     content = ""
     full_content = ""
+    content = input("Enter new line of content: ")
 
-    while content == "stop":
+    while content != "stop":
         full_content += f"{content}\n"
         content = input("Enter new line of content: ")
 

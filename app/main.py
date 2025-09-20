fecfcee2bee124e3794c2168cc3f765a6b9f5110
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-09-20 18:46:39 +0300

    fix a problem9

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 def main():
+    # Запитуємо у користувача ім'я файлу
     file_name = input("Enter name of the file: ") + ".txt"
     content = []
 
+    # Збираємо рядки до введення 'stop'
     while True:
         line = input("Enter new line of content: ")
         if line == "stop":
             break
         content.append(line)
 
+    # Записуємо рядки у файл
     with open(file_name, "w") as f:
         for line in content:
             f.write(line + "\n")

dbac771746dcd6fd60da16f9fb18619c073bfd27
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-09-20 18:26:49 +0300

    fix a problem2

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 def main():
+    # Запитуємо у користувача ім'я файлу
+    file_name = input("Enter name of the file: ").strip()
+    if not file_name:
+        print("File name cannot be empty.")
+        return
+    file_name += ".txt"
+
     try:
-        # Запитуємо у користувача ім'я файлу
-        file_name = input("Enter name of the file: ").strip()
-        if not file_name:
-            print("File name cannot be empty.")
-            return
-
-        file_name += ".txt"
-
         # Відкриваємо файл для запису
         with open(file_name, "w") as f:
             while True:
@@ -15,11 +14,9 @@ def main():
                 if line.lower() == "stop":
                     break
                 f.write(line + "\n")
-
         print(f"File '{file_name}' created successfully!")
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    except OSError as e:
+        print(f"Cannot create or write to file: {e}")
 
 
 if __name__ == "__main__":

815e913603039c2c157e7768a579843db7a084c5
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-09-20 19:20:15 +0300

    fix a problem03

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 def main():
-    # write your code here
-    pass
+    # Питаємо ім'я файлу
+    file_name = input("Enter name of the file: ") + ".txt"
+
+    content = []
+
+    # Запит рядків у циклі
+    while True:
+        line = input("Enter new line of content: ")
+        if line == "stop":
+            break
+        content.append(line)
+
+    # Запис у файл
+    with open(file_name, "w") as f:
+        for line in content:
+            f.write(line + "\n")
 
 
 if __name__ == "__main__":
     main()
+

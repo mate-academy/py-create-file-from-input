47cfa910663747bda835cfbf0dba5ae1d555eab3
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-09-20 18:58:13 +0300

    fix a problem01

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,11 @@
 import sys
 import os
 
-# Додаємо корінь проєкту у PYTHONPATH, щоб Python бачив пакет app
+# Додаємо корінь проєкту у sys.path, щоб Python бачив пакет 'app'
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app.main import main
 
-def test_placeholder():
-    # Тут можна буде зробити мок вводу для pytest
-    # Для автотестів достатньо, щоб імпорт працював
+def test_import():
+    # Простий тест, перевіряє, що main() імпортується і є викликабельною функцією
     assert callable(main)

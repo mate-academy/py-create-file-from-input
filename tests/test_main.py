6c6107eac1ebf85e3207547129498d10e4da4ed5
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-09-20 19:00:21 +0300

    fix a problem02

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,5 +7,5 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from app.main import main
 
 def test_import():
-    # Простий тест, перевіряє, що main() імпортується і є викликабельною функцією
+    # Перевіряємо, що функція main імпортується і викликається
     assert callable(main)

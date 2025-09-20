6e9c37e7eb36632847a6bee34420f6a0a79f33b7
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-09-20 18:22:07 +0300

    task is completed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
 def main():
-    # write your code here
-    pass
+    # Запитуємо у користувача ім'я файлу
+    file_name = input("Enter name of the file: ") + ".txt"
+
+    lines = []
+
+    # Просимо користувача вводити рядки у циклі
+    while True:
+        line = input("Enter new line of content: ")
+        if line.lower() == "stop":
+            break
+        lines.append(line)
+
+    # Створюємо файл і записуємо туди всі рядки
+    with open(file_name, "w") as f:
+        for line in lines:
+            f.write(line + "\n")
+
+    print(f"File '{file_name}' created successfully!")
 
 
 if __name__ == "__main__":

f82f48d6a151b4281b6e1aafc67db5b64487e862
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-09-23 14:44:17 +0300

    fix a problem10

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 def main():
-    # Запитуємо назву файлу
     file_name = input("Enter name of the file: ").strip()
     if not file_name.endswith(".txt"):
         file_name += ".txt"
 
-    # Збираємо вміст
     content_lines = []
     while True:
         line = input("Enter new line of content: ")
@@ -12,6 +10,5 @@ def main():
             break
         content_lines.append(line)
 
-    # Записуємо у файл
     with open(file_name, "w", encoding="utf-8") as f:
         f.write("\n".join(content_lines))

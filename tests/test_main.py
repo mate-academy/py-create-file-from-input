527fd8a6ebea90914c41eb50f334ca7209ab8fd0
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-09-23 13:13:15 +0300

    fix a problem06

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,11 @@
 import sys
 import os
+import pytest
 
-# Додаємо корінь проєкту у sys.path, щоб Python бачив пакет 'app'
+from pytest import MonkeyPatch
+
+# додаємо корінь проєкту у sys.path, щоб Python бачив пакет `app`
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app.main import main
 
-def test_import():
-    # Перевіряємо, що функція main імпортується і викликається
-    assert callable(main)

7d13b515837230e03c956528b718b22cba3f096c
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2025-09-25 23:37:47 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-def main():
-    # write your code here
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+    command_storage = []
+    while True:
+        user_command = input("Enter new line of content: ")
+        if user_command == "stop":
+            break
+        command_storage.append(user_command)
 
+    with open(f"{file_name}.txt", "a") as f:
+        for command in command_storage:
+            f.write(command + "\n")
 
 
 if __name__ == "__main__":

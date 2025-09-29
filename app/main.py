bc0ba72d523f504a64abb96d385d81c1a8f91eac
Sasha <alex.acmate@gmail.com>
2025-09-29 21:21:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def main() -> None:
     file_name = str(input("Enter name of the file: "))
     with open(f"{file_name}.txt", "w") as f:
-        f.write(f"{file_name}\n")
         while True:
             content = str(input("Enter new line of content: "))
             if content.lower() != "stop":

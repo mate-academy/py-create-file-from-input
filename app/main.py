d8f04310829ebf611d8d1810b8ba3a98c085f55e
Maksym Dazidov <maksymdazidov@gmail.com>
2025-10-10 17:19:37 +0200

    file 3

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 def main() -> None:
-    name = input("Enter name of the file")
+    name = input("Enter name of the file: ")
     with open(f"{name}.txt", "a") as new_file:
         while True:
-            line = input("Enter new line of content\n")
+            line = input("Enter new line of content: ")
             if line == "stop":
                 break
-            new_file.write(line)
+            new_file.write(f"{line}\n")
 
 
 if __name__ == "__main__":

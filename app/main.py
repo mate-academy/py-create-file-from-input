a7122fbcfba9a30884b159eb86bd8f06e21c64d2
Ihor Zasimenko <boxletter2011@gmail.com>
2025-10-10 19:23:25 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,16 +1,12 @@
-def create_text_file() -> None:
-    file_name: str = input("Enter name of the file: ").strip()
-    lines: list[str] = []
+def main() -> None:
+    file_name: str = input("Enter name of the file: ")
+    content: list[str] = []
 
     while True:
         line: str = input("Enter new line of content: ")
         if line.lower() == "stop":
             break
-        lines.append(line)
+        content.append(line)
 
     with open(f"{file_name}.txt", "w", encoding="utf-8") as file:
-        file.write("\n".join(lines))
-
-
-if __name__ == "__main__":
-    create_text_file()
+        file.write("\n".join(content))

a68c90fd3099cec94e2bfc967d241584f108f9ab
artem <artemasarkisan598@gmail.com>
2025-10-24 22:35:07 +0300

    py -Create file from input

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
-def main():
+def main(content: list, file_basename: str) -> None:
     # write your code here
-    pass
+    file_basename = input("Enter name of the file:")
+    print(f"hello word {file_basename}")
+
+    with open(f"{file_basename}.txt", "a") as f:
+        while True:
+            line = input("Enter new line of content:")
+            if line == "stop":
+                break
+            f.write(line + "\n")
 
 
 if __name__ == "__main__":
-    main()
+    main([], "")

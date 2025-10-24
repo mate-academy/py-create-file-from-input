0327bb8b28d1e4e169693ddf9a5181266ef95758
Oleksandr Plaksa <plaksa.sasha@gmail.com>
2025-10-24 19:34:25 +0300

    Refactor: Improve input loop logic

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 def main() -> None:
     file_name = input("Enter name of the file: ")
-    with open(file_name + ".txt", "w") as file1:
-        text = ""
-        while text != "stop":
+
+    with open(file_name + ".txt", "a") as f:
+        while True:
             text = input("Enter new line of content: ")
-            if text != "stop":
-                file1.write(text + "\n")
+            if text.lower() == "stop":
+                break
+            f.write(text + "\n")
 
 
 if __name__ == "__main__":
     main()
+    # refactored 2.20.24

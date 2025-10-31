21082963abe0094b3c394163d6780c03a4abbe50
Tarasov Maksym <maksimmm45555@gmail.com>
2025-11-01 00:32:09 +0200

    solved py-create-file-from-input task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    name = input("Enter name of the file: ")
+    if not name:
+        print("File name cannot be empty.")
+        return
+
+    lines = []
+    while True:
+        line = input("Enter new line of content: ")
+        if line.lower() == "stop":
+            break
+        lines.append(line + "\n")
+
+    with open(f"{name}.txt", "w") as f:
+        f.writelines(lines)
 
 
 if __name__ == "__main__":

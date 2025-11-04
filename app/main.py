8e4d34c034226d2597a7f3c82c15d27403be3dfb
Vadym Vynokur <king.krollie@gmail.com>
2025-11-04 19:23:38 +0200

    add solution

--- app/main.py
+++ app/main.py
@@ -1,17 +1,16 @@
-def main():
+def main() -> None:
     file_name = input("Enter name of the file: ")
     if not file_name.endswith(".txt"):
         file_name += ".txt"
 
     print("To save and exit print stop")
 
-    with open(file_name, 'w') as f:
+    with open(file_name, "w") as f:
         while True:
             input_str = input("Enter new line of content: ")
             if input_str == "stop":
                 break
             f.write(input_str + "\n")
-
 
 
 if __name__ == "__main__":

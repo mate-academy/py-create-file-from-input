c082fb4c7bc4be94f2e1f4b5339d0d15572353fe
Nikita <hkorobko@gmail.com>
2025-11-09 00:29:25 +0200

    Create a file from input

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    result = input("Enter name of the file: ") + ".txt"
+    lines = []
+
+    while True:
+        line = input("Enter new line of content: ")
+        if line == "stop":
+            break
+        lines.append(line)
+
+    with open(result, "w") as f:
+        for line in lines:
+            f.write(line + "\n")
 
 
 if __name__ == "__main__":

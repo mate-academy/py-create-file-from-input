942ab97eee4bdbd13bc543cb9c989a93cb81ace7
Yan Tumar <jantumar.unity@gmail.com>
2025-11-09 13:29:53 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+
+    file_name: str = input("Enter name of the file: ")
+    full_name: str = file_name + ".txt"
+
+    lines: list[str] = []
+
+    while True:
+        line: str = input("Enter new line of content: ")
+        if line.lower() == "stop":
+            break
+        lines.append(line)
+
+    file = open(full_name, "w", encoding="utf-8")
+    for line in lines:
+        file.write(line + "\n")
+    file.close()
+
+    print(f'File "{full_name}" was created successfully!')
 
 
 if __name__ == "__main__":

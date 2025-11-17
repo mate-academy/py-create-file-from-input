db93b936c1d9d3f623898900e0cf80b21a2613fa
tomech <109527747+tomech4@users.noreply.github.com>
2025-11-17 23:15:30 +0100

    complete function for input form

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 def main():
-    # write your code here
-    pass
+    file_name = input("Enter name of the file: ")
+    lines = []
+    while True:
+        content = input("Enter new line of content: ")
+        if content == "stop":
+            break
+        lines.append(content)
+
+    with open(f"{file_name}.txt", "a") as f:
+        for line in lines:
+            f.write(f"{line}\n")
 
 
 if __name__ == "__main__":

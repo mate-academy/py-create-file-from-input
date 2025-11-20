5e5703f4c2d955677e35b204552d8fb3706bc8f8
Vova <vovakucin082@gmail.com>
2025-11-20 10:47:59 +0100

    code write

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ def main() -> None:
     input_user = input("Enter name of the file: ")
     with open(input_user + ".txt", "w") as file:
         while input_user != "stop\n":
-            input_user = f"{input("Enter new line of content: ")}\n"
-            print(input_user)
+            input_user = input("Enter new line of content: ")
+            input_user += "\n"
             file.write(input_user if input_user != "stop\n" else "")
 
 

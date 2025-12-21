8e91b8f6ac7d0977826ccc79366ac040c8d3a3b3
Artem <artem90078@gmail.com>
2025-12-21 10:08:46 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def main():
+def main() -> None:
     file_base_name = input("Enter name of the file: ")
     content_lines = []
 
@@ -8,13 +8,16 @@ def main():
             break
         content_lines.append(line)
 
-    filename = file_base_name if file_base_name.endswith(".txt") else file_base_name + ".txt"
+    filename = file_base_name\
+        if file_base_name.endswith(".txt")\
+        else file_base_name + ".txt"
 
     try:
         with open(filename, "w", encoding="utf-8") as file:
             file.write("\n".join(content_lines))
     except Exception as e:
         print(f"An error occurred: {e}")
+
 
 if __name__ == "__main__":
     main()

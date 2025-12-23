57ea4e8e101e5e32d69dcd19e9e4477090661add
LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>
2025-12-23 19:43:49 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,36 +1,20 @@
-<<<<<<< HEAD
 def main() -> None:
-    file_name = ""
-    while not file_name:
-        file_name = input("Enter name of the file: ").strip()
+file_name = ""
+while not file_name:
+file_name = input("Enter name of the file: ").strip()
 
-    if not file_name.lower().endswith(".txt"):
-        file_name += ".txt"
+if not file_name.lower().endswith(".txt"):
+    file_name += ".txt"
 
-    lines = []
-    while True:
-        line = input("Enter new line of content: ")
-        if line.strip().lower() == "stop":
-            break
-        lines.append(line)
-=======
-from datetime import datetime
-from time import sleep
+lines = []
+while True:
+    line = input("Enter new line of content: ")
+    if line.strip().lower() == "stop":
+        break
+    lines.append(line)
 
+with open(file_name, "w") as f:
+    f.write("\n".join(lines))
 
-def main():
-    while True:
-        now = datetime.now()
-        filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
-        content = now.strftime("%Y-%m-%d %H:%M:%S")
->>>>>>> dd79131eca609e1ad65f2401148ca7e840e02c29
-
-        with open(filename, "w") as f:
-            f.write(content)
-
-        print(f"{content} {filename}")
-        sleep(1)
-
-
-if __name__ == "__main__":
-    main()
+f name == "main":
+main()

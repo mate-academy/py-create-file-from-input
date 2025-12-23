320435cc88d8edee22be354141eec640d3cc9183
LUAN ESTIFER RODRIGUES PEREIRA <113989498+Zsubzeroz@users.noreply.github.com>
2025-12-23 19:12:54 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,17 @@
 def main():
     file_name = input("Enter name of the file: ").strip()
-    if not file_name.endswith(".txt"):
+    if not file_name.lower().endswith(".txt"):
         file_name += ".txt"
-
+    
     lines = []
     while True:
-        line = input("Enter new line of content: ")
-        if line == "stop":
+        entry = input("Enter new line of content: ")
+        if entry.strip().lower() == "stop":
             break
-        lines.append(line)
-
+        lines.append(entry)
+    
     with open(file_name, "w") as f:
-        f.write("\n".join(lines))
-
+        f.writelines(line + "\n" for line in lines)
 
 if __name__ == "__main__":
     main()

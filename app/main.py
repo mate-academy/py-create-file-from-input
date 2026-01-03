285af86ffc11e32650b66f3af3395ef7702a12eb
Your Name <allanaumenko@Allas-MacBook-Air.local>
2026-01-03 16:07:14 +0800

    Create file from user input

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 def main():
-    # write your code here
-    pass
+    file_name = input("Enter name of the file: ").strip()
+    full_name = f"{file_name}.txt"
+
+    lines = []
+    while True:
+        line = input("Enter new line of content: ")
+        if line == "stop":
+            break
+        lines.append(line)
+
+    with open(full_name, "w", encoding="utf-8") as f:
+        if lines:
+            f.write("\n".join(lines))
 
 
 if __name__ == "__main__":

81f74f8bac9c859f1c71f60376bc69140535887a
Vadym Kravchenko <vadymkravchenko.us@gmail.com>
2026-01-09 11:27:16 -0500

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 def main():
-    # write your code here
-    pass
+    file_name = input("name of the file: ").strip()
+
+    if not file_name.endswith(".txt"):
+        file_name += ".txt"
+
+    content: list[str] = []
+
+    while True:
+        line = input("Enter new line of content: ")
+        if line == "stop":
+            break
+        content.append(line)
+
+    with open(file_name, "w") as file:
+        file.write("\n".join(content))
 
 
 if __name__ == "__main__":

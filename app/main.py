a7137220ca363361c03289b902e1cf0a465cce52
Bohdan Borsuk <barsuchellos@gmail.com>
2026-01-11 19:41:59 +0000

    fixed

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def main() -> None:
-    name1:str = input("Enter name of the file: ")
+    name1: str = input("Enter name of the file: ")
     with open(f"{name1}.txt", "a") as f:
         while True:
-            line:str = input("Enter new line of content: ")
+            line: str = input("Enter new line of content: ")
             if line == "stop":
                 break
             f.write(f"{line}\n")

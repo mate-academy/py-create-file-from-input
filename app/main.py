70db86186d6334760c773e1f941d939e39df3017
Andriy <andrijbuluk@gmail.com>
2026-01-16 14:43:19 +0100

    Create a file from input

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 def main():
-    # write your code here
-    pass
+    name = input("File name: ")
+
+    if not name.endswith(".txt"):
+        name += ".txt"
+
+    with open(name, "w") as f:
+        while True:
+            file = input("file content: ")
+            if file == "stop":
+                break
+            f.write(file + "\n")
 
 
 if __name__ == "__main__":

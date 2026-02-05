cca42f1081465722197f94647a44d98bf1d04a9b
Melezhyk Oleksandr <melezhyk02@gmail.com>
2026-02-05 13:57:28 +0200

    implement pt-create-file-from-input

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    f_name = input("Enter name of the file: ")
+    if ".txt" not in f_name:
+        f_name = f_name + ".txt"
+    with open(f_name, "w") as f:
+        while True:
+            text = input("Enter new line of content: ")
+            if text == "stop":
+                break
+            f.write(text + "\n")
 
 
 if __name__ == "__main__":

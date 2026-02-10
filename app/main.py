1a64a4caf1cccd71e6885904f6031c2005a864d8
Andrii Shukalo <shukaload@gmail.com>
2026-02-10 19:37:35 +0200

    Implement small app for interactive create and fill with data files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ")
 
+    if not file_name.endswith(".txt"):
+        file_name += ".txt"
 
-if __name__ == "__main__":
-    main()
+    lines = []
+
+    while True:
+        line = input("Enter new line of content: ")
+        if line == "stop":
+            break
+        lines.append(line)
+
+    content = "\n".join(lines)
+
+    with open(file_name, "w") as f:
+        f.write(content)

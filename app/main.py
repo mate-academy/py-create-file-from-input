b597fc4d17e0a7a0dbbfee89c7c43724c685f8a6
Mykhailo Chivha <chivgamihail@gmail.com>
2026-02-10 14:08:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:  # Переконайся, що назва саме така
+    file_name = input("Enter name of the file: ")
+    full_name = f"{file_name}.txt"
+
+    content_lines = []
+
+    while True:
+        line = input("Enter new line of content: ")
+        if line.lower() == "stop":
+            break
+        content_lines.append(line)
+
+    with open(full_name, mode="w", encoding="utf-8") as file:
+        # Важливо: тести можуть очікувати перенос рядка в кінці
+        for line in content_lines:
+            file.write(line + "\n")
 
 
 if __name__ == "__main__":

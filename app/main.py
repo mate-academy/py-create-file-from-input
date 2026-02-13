74ed2180922f5306b9f141540e889ae75a2332ef
Jerzy Joniec <j.joniec5@gmail.com>
2026-02-13 09:57:58 +0100

    „final”

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    name = input("Enter name of the file: ")
+    file_user = open(f"{name}.txt", "a")
+    while True:
+        content = input("Enter new line of content: ")
+        if content == "stop":
+            break
+        file_user.write(content + "\n")
+    file_user.close()
 
 
 if __name__ == "__main__":

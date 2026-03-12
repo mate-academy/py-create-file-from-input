f8d8ced5e8b6837a5c8648fefb63fa11eeea7a8c
LASC-dev <lucas.a.s.casas@outlook.es>
2026-03-12 17:50:16 -0300

    feat: implement main fuction

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    """
+    This function prompts the user to enter a file name and allows them
+    to write lines of content to the file until they type "stop".
+    """
+    file_name = input("Enter name of the file: ")
+
+    if not file_name.endswith(".txt"):
+        file_name += ".txt"
+
+    with open(file_name, "w") as file:
+        while True:
+            line = input("Enter new line of content: ")
+            if line.lower() == "stop":
+                break
+            file.write(line + "\n")
 
 
 if __name__ == "__main__":

2534cf1c53a8ae201ffe6005a3a123b3f96f60b0
Denys Baliuk <denuk14228@gmail.com>
2026-03-17 23:54:21 +0200

    first try

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Введіть ім`я файлу: ")
+    content_lines = []
+
+    while True:
+        line = input("Введіть текст: ")
+        if line == "stop":
+            break
+        content_lines.append(line + "\n")
+
+    full_name = f"{file_name}.txt"
+
+    with open(full_name, "w", encoding="utf-8") as file:
+        file.writelines(content_lines)
+
+    print(f"Файл '{full_name}' успішно створено!")
 
 
 if __name__ == "__main__":

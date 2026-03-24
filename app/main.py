1da7f4ce1fe99b163cd8a6a2fb89ed33eb9c9a16
Rostyk <rostik.durjagin@gmail.com>
2026-03-24 18:20:48 +0200

    Compete

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+
+    lines = []
+
+    while True:
+        line = input("Enter new line of content: ")
+        if line == "stop":
+            break
+        lines.append(line)
+
+    if not file_name.endswith(".txt"):
+        file_name += ".txt"
+
+    with open(file_name, "w") as file:
+        file.write("\n".join(lines))
 
 
 if __name__ == "__main__":

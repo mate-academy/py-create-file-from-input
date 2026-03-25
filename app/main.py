19c950a79f05c1d372e7125dcbd67c791ab6132a
Vijay Chand <vijaychand6979@googlemail.com>
2026-03-25 16:44:07 +0000

    added code

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+
+    output_file = open(f"{file_name}.txt", "w")
+
+    while True:
+        entered_text = input("Enter new line of content: ")
+
+        if entered_text == "stop":
+            break
+
+        output_file.write(entered_text + "\n")
+
+    output_file.close()
 
 
 if __name__ == "__main__":

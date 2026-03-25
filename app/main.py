977ea69afea892bef50ffbe4ba623ec6be443a7b
Anton Gaievyi <anton.gaievyi@gmail.com>
2026-03-25 17:26:34 +0100

    implement py-create-file-from-input

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def main():
+def main() -> None:
     file_name = input("Enter name of the file: ")
     if not file_name.endswith(".txt"):
         file_name += ".txt"
@@ -13,6 +13,7 @@ def main():
     with open(file_name, "w") as file:
         for line in lines:
             file.write(line + "\n")
+
 
 if __name__ == "__main__":
     main()

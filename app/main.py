0c1e30ccc578697c9d1abfb71d04dac603ab48ce
Neyrony <sserebriy@gmail.com>
2026-03-30 17:04:04 +0300

    Implemented solution for py-create-from-input

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-def main():
-    # write your code here
-    pass
-
-
-if __name__ == "__main__":
-    main()
+def main() -> None:
+    file_name = input("Enter name of the file: ") + ".txt"
+    while True:
+        with open(file_name, "a") as file:
+            line = input("Enter new line of content: ")
+            if line == "stop":
+                break
+            file.write(line + "\n")

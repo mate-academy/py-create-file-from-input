c956e7a0e9ef4e718bb1285e7fb9b8d9324441cf
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-04-01 11:08:26 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def main():
+def main() -> None:
     file_name = input("Enter name of the file: ")
     if not file_name.endswith(".txt"):
         file_name += ".txt"
@@ -8,10 +8,6 @@ def main():
             if line.lower() == "stop":
                 break
             f.write(f"{line}\n")
-
-
-
-
 
 if __name__ == "__main__":
     main()

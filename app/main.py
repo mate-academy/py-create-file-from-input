9c2d1ccf6ccdb3779e0fad48afc1f4591396b081
Ivan Synytskyi <ivansinickij29@icloud.com>
2026-04-04 13:05:57 +0200

    done12

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    name = input("Enter name of the file: ")
+    if not name.endswith(".txt"):
+        name = name + ".txt"
+    lines = []
+    while True:
+        line = input("Enter new line of content: ")
+        if line == "stop":
+            break
+        lines.append(line)
+
+    with open(name, "w", encoding="utf-8") as f:
+        f.write("\n".join(lines))
 
 
 if __name__ == "__main__":

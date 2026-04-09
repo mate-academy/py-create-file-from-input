5478e792581f91a6bae98a1ee4efb21eec57c6d9
gunsts <137734013+gunsts@users.noreply.github.com>
2026-04-09 21:35:45 +0700

    Solution

--- app/main.py
+++ app/main.py
@@ -3,8 +3,6 @@ def main() -> None:
     if len(file_name) > 0:
         file_name = file_name.strip() + ".txt"
         file_output = open(file_name, "w")
-        file_output.write(f"File name: {file_name}\n")
-        file_output.write("File content:\n")
         while True:
             input_line = input("Enter new line of content: ")
             if "stop" in input_line:

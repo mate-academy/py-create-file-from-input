4a0ab63bf194c6443a52f74d7f29a90a5c41a469
Igor <zalugin.iv2@gmail.com>
2026-04-15 14:45:56 +0200

    all tests passed

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
-def main():
-    # write your code here
+def main() -> None:
+    with open(input("Enter name of the file: ") + ".txt", "w") as f:
+        while True:
+            line = input("Enter new line of content: ")
+            if line == "stop":
+                break
+            f.write(line + "\n")
     pass
 
 

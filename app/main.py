8eed60d25d147b9796d65cb3f6c9ed73ec38bddc
Mykyta Titar <mykyta.titar@gmail.com>
2026-05-09 20:29:45 +0300

    Solution1

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    lines = []
+    data_of_user = input("Enter name of the file: ")
+    new_line = input("Enter new line of content: ")
+    while new_line != "stop":
+        lines.append(new_line + "\n")
+        new_line = input("Enter new line of content: ")
+
+    with open(f"{data_of_user}.txt", "w") as file:
+        file.write("".join(lines))
 
 
 if __name__ == "__main__":

b395bb0e84db5670d864832ee72f09da0606671e
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-05-11 16:50:28 +0300

    Fixed flake8

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
-def main():
+def main() -> None:
     name = input("Enter name of the file: ") + ".txt"
-    with open (name,'w') as f:
+    with open(name, "w") as f:
 
         while True:
             line = input("Enter new line of content: ")
             if line == "stop":
                 break
             f.write(line + "\n")
+
 
 if __name__ == "__main__":
     main()

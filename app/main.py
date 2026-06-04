0989b075bec862d5515275ad23a5a46cbdb12eb9
Dmytro <dmitryibezkrovnyi@gmail.com>
2026-06-04 16:06:20 +0200

    first solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 def main():
-    # write your code here
-    pass
+    file_name = input("Ented namae of the file")
+
+    content = []
+
+    while True:
+        line = input("Enter new line of content: ")
+
+        if line == "stop":
+            break
+        content. append(line)
+
+    with open(f"{file_name}.txt", "w") as file:
+        file.write("\n".join(content))
 
 
 if __name__ == "__main__":

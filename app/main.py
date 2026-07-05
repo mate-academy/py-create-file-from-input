617fcbe5e31a556a4ff52a482ea25c2acba2dce7
Teashrock <kajitsu22@gmail.com>
2026-07-05 18:48:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    with open(f"{input('Enter name of the file: ')}.txt", "w") as uinput:
+        while True:
+            chunk = input("Enter new line of content: ")
+            if chunk == "stop":
+                break
+            uinput.write(f"{chunk}\n")
 
 
 if __name__ == "__main__":

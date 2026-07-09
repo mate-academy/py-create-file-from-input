a7d7c5887aa06247b571f015a2bfbd588b29f28d
Anderson <anderson.medeiros3011@gmail.com>
2026-07-09 18:22:02 -0300

    feat: implementa escrita de arquivo dentro da funcao main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 def main():
-    # write your code here
-    pass
+    nome_arquivo = input("Digite o nome do arquivo: ")
+    nome_arquivo += ".txt"
+    conteudo = []
+
+    while True:
+        linha = input("Digite uma linha: ")
+        if linha == "stop":
+            break
+        conteudo.append(linha)
+
+    with open(nome_arquivo, "w") as arquivo:
+        for linha in conteudo:
+            arquivo.write(linha + "\n")
 
 
 if __name__ == "__main__":

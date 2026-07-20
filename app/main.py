3fafb06ac65bc225cdc7cc3f3044b4e513b8d600
nalyvaiko-denys <nalyvaikodenys.dev@gmail.com>
2026-07-20 22:39:58 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ").strip()
+
+    if not file_name.endswith(".txt"):
+        file_name += ".txt"
+
+    content_lines = []
+
+    while True:
+        line = input("Enter new line of content: ")
+        if line.strip().lower() == "stop":
+            break
+
+        content_lines.append(line)
+
+    try:
+        with open(file_name, "w", encoding="utf-8") as file:
+            file.write("\n".join(content_lines))
+        print(f"\n# File '{file_name}' was successfully created!")
+    except Exception as e:
+        print(f"An error occurred while creating the file: {e}")
 
 
 if __name__ == "__main__":

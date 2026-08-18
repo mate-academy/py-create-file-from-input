28b3fce5e343eab893f7933abd1ad915419089ca
Artur Neruk <anerukpfk@gmail.com>
2026-08-18 14:09:48 +0300

    Solve create a file from input task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+
+    content = []
+    while True:
+        line = input("Enter new line of content: ")
+        if line == "stop":
+            break
+        content.append(line)
+
+    full_file_name = f"{file_name}.txt"
+
+    with open(full_file_name, "w", encoding="utf-8") as file:
+        for line in content:
+            file.write(f"{line}\n")
 
 
 if __name__ == "__main__":

a72c44e9d205767a6623be907123dd59fa886003
Alexandr <travmagolovu1312@gmail.com>
2026-08-24 15:45:14 +0300

    completed please review

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ").strip()
+    file_name = file_name + ".txt"
+
+    lines = []
+    while True:
+        line = input("Enter new line of content: ")
+        if line.lower() == "stop":
+            break
+        lines.append(line)
+
+    with open(file_name, "w") as f:
+        for line in lines:
+            f.write(line + "\n")
+
+    print(f"File {file_name} created successfully!")
 
 
 if __name__ == "__main__":

23e3d92e14d479cdcf8e85b386997be03870035f
Paulina Marin Gallego <paulinamarin3105@gmail.com>
2026-08-25 06:11:24 -0500

    create main function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-def main():
-    # write your code here
-    pass
+import os
+
+
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+    path_file = os.path.join(os.getcwd(), f"{file_name}.txt")
+    new_file = open(path_file, "a")
+
+    input_text = ""
+    while input_text != "stop":
+        input_text = input("Enter new line of content: ")
+        if input_text == "stop":
+            break
+        new_file.write(f"{input_text}\n")
+
+    new_file.close()
 
 
 if __name__ == "__main__":

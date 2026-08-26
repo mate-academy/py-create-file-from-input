802f7e59bac8401e7709d6b5157d1cc35064e024
brit2pac <belo4ka.valeri@gmail.com>
2026-08-26 13:38:50 +0000

    Solution fix

--- app/main.py
+++ app/main.py
@@ -1,17 +1,14 @@
-def main():
+def main() -> None:
     file_name = input("Enter name of the file: ").strip()
-
     lines = []
     while True:
         line = input("Enter new line of content: ")
         if line == "stop":
             break
         lines.append(line)
-
     output_name = f"{file_name}.txt"
     with open(output_name, "w") as f:
         f.write("\n".join(lines))
-
     print(f'Created "{output_name}" with {len(lines)} line(s).')
 
 

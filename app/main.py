505ba5112f568b90845410cb854fc0d8706364c3
Aleksandr Majlo <aleksandrmajlo@MacBook-Pro-Aleksandr.local>
2026-09-03 09:41:41 +0300

    fix: all tests passing

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+    file_lines = []
+    while True:
+        content = input("Enter new line of content: ")
+        if content == "stop":
+            break
+        file_lines.append(content)
+    with open(file_name + ".txt", "w") as file:
+        for line in file_lines:
+            file.write(line + "\n")
 
 
 if __name__ == "__main__":

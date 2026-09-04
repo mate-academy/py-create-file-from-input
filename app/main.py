ffa24b435e37cc41a7b89a134e96ecd70d22bf28
Khrystyna <tkachristina079@gmail.com>
2026-09-05 01:09:50 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+    contex = []
+    while True:
+        line = input("Enter new line of content: ")
+
+        if line == "stop":
+            break
+
+        contex.append(line)
+
+    with open(f"{file_name}.txt", "w") as file:
+        for line in contex:
+            file.write(line + "\n")
 
 
 if __name__ == "__main__":

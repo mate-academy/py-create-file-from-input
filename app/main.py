0504ee5e9cbb51a8f5af3a077bf73d65cf86ba41
Pavlo_Vizhniak <pavlovizhniak@gmail.com>
2026-09-05 12:12:41 +0200

    feat: init main function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = str(input("Enter name of the file: "))
+    content = str(input("Enter new line of content: "))
+
+    with open(file_name + ".txt", "a") as file:
+        while content.lower() != "stop":
+            file.write(content + "\n")
+            content = str(input("Enter new line of content: "))
 
 
 if __name__ == "__main__":
